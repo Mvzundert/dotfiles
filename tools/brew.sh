a190afe3b0d37ebd3bc36b4d00fa85d0a4c88c6e
Marius van Zundert <marius.vanzundert@webcolorsgroup.com>
2017-05-11 13:32:16 +0200

    Updated installer and added brew.sh for installing on OSX

---
+++ tools/brew.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+if test ! $(which brew); then
+    echo "Installing homebrew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"gs should pass through the the `brew list check`
+
+fi
+
+echo -e "\n\nInstalling homebrew packages..."
+echo "=============================="
+
+formulas=(
+    ack
+    icu4c
+    mysql
+    reattach-to-user-namespace
+    autoconf
+    jpeg
+    node
+    ruby
+    automake
+    libevent
+    openssl
+    sqlite
+    coreutils
+    libgpg-error
+    openssl@1.1
+    tmux
+    ctop
+    libksba
+    pcre
+    unixodbc
+    drush
+    libpng
+    perl
+    vim
+    freetype
+    libtool
+    php70
+    wget
+    fswatch
+    libxml2
+    pkg-config
+    zsh
+    gdbm
+    libyaml
+    python
+    zsh-completions
+    gettext
+    lnav
+    readline
+)
+
+for formula in "${formulas[@]}"; do
+    if brew list "$formula" > /dev/null 2>&1; then
+        echo "$formula already installed... skipping."
+    else
+        brew install $formula
+    fi
+done
