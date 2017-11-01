9083ec1fe55bbb4c3a2288fe4ae5c3fb80498eab
Marius van Zundert <marius.vanzundert@webcolorsgroup.com>
2017-11-01 20:19:17 +0100

    Added DNSMASQ to brew list for installing

--- tools/brew.sh
+++ tools/brew.sh
@@ -3,7 +3,6 @@
 if test ! $(which brew); then
     echo "Installing homebrew"
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"gs should pass through the the `brew list check`
-
 fi
 
 echo -e "\n\nInstalling homebrew packages..."
@@ -18,6 +17,7 @@ formulas=(
     jpeg
     node
     ruby
+    dnsmasq
     automake
     libevent
     openssl
