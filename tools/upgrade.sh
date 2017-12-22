2896f809d51d50c29c68022c5eabd07d2b460201
Sven Vissers <sven.vissers@webcolorsgroup.com>
2017-12-22 11:42:14 +0100

    updated logo

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -6,9 +6,8 @@ then
   printf '%s\n' '________          __    _____.__.__                 '
   printf '%s\n' '\______ \   _____/  |__/ ____\__|  |   ____   ______'
   printf '%s\n' ' |    |  \ /  _ \   __\   __\|  |  | _/ __ \ /  ___/'
-  printf '%s\n' ' |    `   (  <_> )  |  |  |  |  |  |_\  ___/ \___ \ '
-  printf '%s\n' '/_______  /\____/|__|  |__|  |__|____/\___  >____  >'
-  printf '%s\n' '        \/                                \/     \/ '
+  printf '%s\n' ' |    `   (  (_) )  |  |  |  |  |  |_\  ___/ \___ \ '
+  printf '%s\n' '/_______  /\____/|__|  |__|  |__|____/\___ ) ______)'
   printf "%s\n" "Hooray! Dotfiles have been updated and/or is at the current version."
 else
   printf "%s\n" 'There was an error updating. Try again later?'
