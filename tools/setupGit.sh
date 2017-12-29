bac5cf3fd65772ee2a1eb42d30a17ce7130166bb
Marius van Zundert <marius.vanzundert@youaredigital.nl>
2017-12-29 12:01:18 +0100

    Added pre-commit git hook(s) and setup

---
+++ tools/setupGit.sh
@@ -0,0 +1,57 @@
+#! /bin/bash
+
+# echo " "
+# echo "======================================="
+# echo "========= OS dependencies ============"
+# echo "======================================="
+# echo " "
+if [ "$(uname)" == "linux-gnu" ]; then
+    echo -e "\n\nRunning on Linux"
+    echo -e "\n\nCannot setup git (yet)"
+fi
+
+if [ "$(uname)" == "Darwin" ]; then
+    echo -e "\n\nRunning on OSX"
+
+    if test ! $(which brew); then
+        echo "Installing homebrew"
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"gs should pass through the the `brew list check`
+    fi
+
+    echo -e "\n\nInstalling homebrew packages..."
+    echo "=============================="
+
+    formulas=(
+        git
+        php-code-sniffer
+        phpmd
+    )
+
+    for formula in "${formulas[@]}"; do
+        if brew list "$formula" > /dev/null 2>&1; then
+            echo "$formula already installed... skipping."
+        else
+            brew install $formula
+        fi
+    done
+
+fi
+
+# echo " "
+# echo "======================================="
+# echo "========= githooks ===================="
+# echo "======================================="
+# echo " "
+DIR="$HOME/.git-templates"
+
+
+if [ -L $DIR ]
+then
+    # echo "$DIR directory exists!"
+    echo "git-templates are installed...skipping"
+else
+    #  echo "$DIR directory created"
+    echo "git-templates have been installed"
+    # Symlink git-templates  to .git-templates folder.
+    ln -s $HOME/dotfiles/tools/git-templates $HOME/.git-templates
+fi
