1e1357c601006b8ae7807c947fdbf9aa60caefc5
Marius van Zundert <Marius.vanzundert@gmail.com>
2019-01-03 13:15:27 +0100

    Did a pass on autocompletion (its a bit better) and did some speed optimizations

--- zsh/plugins/history.zsh
+++ zsh/plugins/history.zsh
@@ -1,28 +1,50 @@
+## History command configuration
+# remove older duplicate entries from history
+setopt hist_ignore_all_dups
+# remove superfluous blanks from history items
+setopt hist_reduce_blanks 
+# save history entries as soon as they are entered
+setopt inc_append_history
+# share history between different instances of the shell
+setopt share_history
+# record timestamp of command in HISTFILE
+setopt extended_history
+# delete duplicates first when HISTFILE size exceeds HISTSIZE
+setopt hist_expire_dups_first
+# ignore commands that start with space
+setopt hist_ignore_space
+# show command with history expansion to user before running it
+setopt hist_verify
+# add commands to HISTFILE in order of execution
+setopt inc_append_history
+# share command history data
+setopt share_history
+
 ## History wrapper
 function omz_history {
-  local clear list
-  zparseopts -E c=clear l=list
+    local clear list
+    zparseopts -E c=clear l=list
 
-  if [[ -n "$clear" ]]; then
-    # if -c provided, clobber the history file
-    echo -n >| "$HISTFILE"
-    echo >&2 History file deleted. Reload the session to see its effects.
-  elif [[ -n "$list" ]]; then
-    # if -l provided, run as if calling `fc' directly
-    builtin fc "$@"
-  else
-    # unless a number is provided, show all history events (starting from 1)
-    [[ ${@[-1]} = *[0-9]* ]] && builtin fc -l "$@" || builtin fc -l "$@" 1
-  fi
+    if [[ -n "$clear" ]]; then
+        # if -c provided, clobber the history file
+        echo -n >| "$HISTFILE"
+        echo >&2 History file deleted. Reload the session to see its effects.
+    elif [[ -n "$list" ]]; then
+        # if -l provided, run as if calling `fc' directly
+        builtin fc "$@"
+    else
+        # unless a number is provided, show all history events (starting from 1)
+        [[ ${@[-1]} = *[0-9]* ]] && builtin fc -l "$@" || builtin fc -l "$@" 1
+    fi
 }
 
 # Timestamp format
 case $HIST_STAMPS in
-  "mm/dd/yyyy") alias history='omz_history -f' ;;
-  "dd.mm.yyyy") alias history='omz_history -E' ;;
-  "yyyy-mm-dd") alias history='omz_history -i' ;;
-  "") alias history='omz_history' ;;
-  *) alias history="omz_history -t '$HIST_STAMPS'" ;;
+    "mm/dd/yyyy") alias history='omz_history -f' ;;
+    "dd.mm.yyyy") alias history='omz_history -E' ;;
+    "yyyy-mm-dd") alias history='omz_history -i' ;;
+    "") alias history='omz_history' ;;
+    *) alias history="omz_history -t '$HIST_STAMPS'" ;;
 esac
 
 ## History file configuration
@@ -30,11 +52,3 @@ esac
 HISTSIZE=50000
 SAVEHIST=10000
 
-## History command configuration
-setopt extended_history       # record timestamp of command in HISTFILE
-setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
-setopt hist_ignore_dups       # ignore duplicated commands history list
-setopt hist_ignore_space      # ignore commands that start with space
-setopt hist_verify            # show command with history expansion to user before running it
-setopt inc_append_history     # add commands to HISTFILE in order of execution
-setopt share_history          # share command history data
