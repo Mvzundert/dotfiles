458749203c837ad5835e1e86296eec4df68050f6
Marius van Zundert <marius.vanzundert@cerberos.dev>
2019-01-07 15:32:15 +0100

    Added extract function

--- zsh/plugins/extract.zsh
+++ zsh/plugins/extract.zsh
@@ -1,71 +1,22 @@
-alias x=extract
-
-extract() {
-    local remove_archive
-    local success
-    local extract_dir
-
-    if (( $# == 0 )); then
-        cat <<-'EOF' >&2
-            Usage: extract [-option] [file ...]
-
-            Options:
-                -r, --remove    Remove archive after unpacking.
-        EOF
-    fi
-
-    remove_archive=1
-    if [[ "$1" == "-r" ]] || [[ "$1" == "--remove" ]]; then
-        remove_archive=0
-        shift
-    fi
-
-    while (( $# > 0 )); do
-        if [[ ! -f "$1" ]]; then
-            echo "extract: '$1' is not a valid file" >&2
-            shift
-            continue
-        fi
-
-        success=0
-        extract_dir="${1:t:r}"
-        case "${1:l}" in
-            (*.tar.gz|*.tgz) (( $+commands[pigz] )) && { pigz -dc "$1" | tar xv } || tar zxvf "$1" ;;
-            (*.tar.bz2|*.tbz|*.tbz2) tar xvjf "$1" ;;
-            (*.tar.xz|*.txz)
-                tar --xz --help &> /dev/null \
-                && tar --xz -xvf "$1" \
-                || xzcat "$1" | tar xvf - ;;
-            (*.tar.zma|*.tlz)
-                tar --lzma --help &> /dev/null \
-                && tar --lzma -xvf "$1" \
-                || lzcat "$1" | tar xvf - ;;
-            (*.tar) tar xvf "$1" ;;
-            (*.gz) (( $+commands[pigz] )) && pigz -d "$1" || gunzip "$1" ;;
-            (*.bz2) bunzip2 "$1" ;;
-            (*.xz) unxz "$1" ;;
-            (*.lzma) unlzma "$1" ;;
-            (*.z) uncompress "$1" ;;
-            (*.zip|*.war|*.jar|*.sublime-package|*.ipsw|*.xpi|*.apk|*.whl) unzip "$1" -d $extract_dir ;;
-            (*.rar) unrar x -ad "$1" ;;
-            (*.7z) 7za x "$1" ;;
-            (*.deb)
-                mkdir -p "$extract_dir/control"
-                mkdir -p "$extract_dir/data"
-                cd "$extract_dir"; ar vx "../${1}" > /dev/null
-                cd control; tar xzvf ../control.tar.gz
-                cd ../data; extract ../data.tar.*
-                cd ..; rm *.tar.* debian-binary
-                cd ..
-            ;;
-            (*)
-                echo "extract: '$1' cannot be extracted" >&2
-                success=1
-            ;;
+# extract any time of compressed file
+function extract {
+    echo Extracting $1 ...
+    if [ -f $1 ] ; then
+        case $1 in
+            *.tar.bz2)   tar xjf $1  ;;
+            *.tar.gz)    tar xzf $1  ;;
+            *.bz2)       bunzip2 $1  ;;
+            *.rar)       rar x $1    ;;
+            *.gz)        gunzip $1   ;;
+            *.tar)       tar xf $1   ;;
+            *.tbz2)      tar xjf $1  ;;
+            *.tgz)       tar xzf $1  ;;
+            *.zip)       unzip $1   ;;
+            *.Z)         uncompress $1  ;;
+            *.7z)        7z x $1  ;;
+            *)        echo "'$1' cannot be extracted via extract()" ;;
         esac
-
-        (( success = $success > 0 ? $success : $? ))
-        (( $success == 0 )) && (( $remove_archive == 0 )) && rm "$1"
-        shift
-    done
+    else
+        echo "'$1' is not a valid file"
+    fi
 }
