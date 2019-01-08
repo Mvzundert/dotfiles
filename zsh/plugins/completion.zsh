218e0bfc5c78957423ea015158407f8b3b46a900
Marius van Zundert <marius.vanzundert@cerberos.dev>
2019-01-08 09:09:29 +0100

    Revised the completion script

--- zsh/plugins/completion.zsh
+++ zsh/plugins/completion.zsh
@@ -1,71 +1,63 @@
 # fixme - the load process here seems a bit bizarre
 zmodload -i zsh/complist
 
-# autocorrect commands
-setopt correct_all 
-# automatically list choices on ambiguous completion
-setopt auto_list 
-# automatically use menu completion
-setopt auto_menu 
-# move cursor to end if word had one match
+WORDCHARS=''
+
+unsetopt menu_complete   # do not autoselect the first completion entry
+unsetopt flowcontrol
+setopt auto_menu         # show completion menu on successive tab press
+setopt complete_in_word
 setopt always_to_end
-
-# Enable approximate completions
-zstyle ':completion:*' completer _complete _ignored _approximate
-zstyle -e ':completion:*:approximate:*' max-errors 'reply=($((($#PREFIX+$#SUFFIX)/3)) numeric)'
-
-# select completions with arrow keys
-zstyle ':completion:*' menu select 
-
-# group results by category
-zstyle ':completion:*' group-name ''
 
 # should this be in keybindings?
 bindkey -M menuselect '^o' accept-and-infer-next-history
 zstyle ':completion:*:*:*:*:*' menu select
 
-# Not case or hyphen sensitive
+# case insensitive (all), partial-word and substring completion  
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
+unset CASE_SENSITIVE HYPHEN_INSENSITIVE
 
 # Complete . and .. special directories
 zstyle ':completion:*' special-dirs true
-# Verbose completion results
-zstyle ':completion:*' verbose true
-# Keep directories and files separated
-zstyle ':completion:*' list-dirs-first true
-# Always use menu selection for `cd -`
-zstyle ':completion:*:*:cd:*:directory-stack' force-list always
-zstyle ':completion:*:*:cd:*:directory-stack' menu yes select
-
-# Pretty messages during pagination
-zstyle ':completion:*' list-prompt '%SAt %p: Hit TAB for more, or the character to insert%s'
-zstyle ':completion:*' select-prompt '%SScrolling active: current selection at %p%s'
-
-# Nicer format for completion messages
-zstyle ':completion:*:descriptions' format '%U%B%d%b%u'
-zstyle ':completion:*:corrections' format '%U%F{green}%d (errors: %e)%f%u'
-zstyle ':completion:*:warnings' format '%F{202}%BSorry, no matches for: %F{214}%d%b'
-
-# Show message while waiting for completion
-zstyle ':completion:*' show-completer true
-
-# Don't complete uninteresting stuff unless we really want to.
-zstyle ':completion:*:functions' ignored-patterns '(_*|pre(cmd|exec)|TRAP*)'
-zstyle ':completion:*:*:*:users' ignored-patterns \
-		adm amanda apache at avahi avahi-autoipd beaglidx bin cacti canna \
-		clamav daemon dbus distcache dnsmasq dovecot fax ftp games gdm \
-		gkrellmd gopher hacluster haldaemon halt hsqldb ident junkbust kdm \
-		ldap lp mail mailman mailnull man messagebus mldonkey mysql nagios \
-		named netdump news nfsnobody nobody nscd ntp nut nx obsrun openvpn \
-		operator pcap polkitd postfix postgres privoxy pulse pvm quagga radvd \
-		rpc rpcuser rpm rtkit scard shutdown squid sshd statd svn sync tftp \
-		usbmux uucp vcsa wwwrun xfs cron mongodb nullmail portage redis \
-		shoutcast tcpdump '_*'
-zstyle ':completion:*' single-ignored show
 
 zstyle ':completion:*' list-colors ''
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
 
-# enable approximate matches for completion
-zstyle ':completion:::::' completer _expand _complete _ignored _approximate
+if [[ "$OSTYPE" = solaris* ]]; then
+  zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm"
+else
+  zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm -w -w"
+fi
 
+# disable named-directories autocompletion
+zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
+
+# Use caching so that commands like apt and dpkg complete are useable
+zstyle ':completion::complete:*' use-cache 1
+zstyle ':completion::complete:*' cache-path $ZSH_CACHE_DIR
+
+# Don't complete uninteresting users
+zstyle ':completion:*:*:*:users' ignored-patterns \
+        adm amanda apache at avahi avahi-autoipd beaglidx bin cacti canna \
+        clamav daemon dbus distcache dnsmasq dovecot fax ftp games gdm \
+        gkrellmd gopher hacluster haldaemon halt hsqldb ident junkbust kdm \
+        ldap lp mail mailman mailnull man messagebus  mldonkey mysql nagios \
+        named netdump news nfsnobody nobody nscd ntp nut nx obsrun openvpn \
+        operator pcap polkitd postfix postgres privoxy pulse pvm quagga radvd \
+        rpc rpcuser rpm rtkit scard shutdown squid sshd statd svn sync tftp \
+        usbmux uucp vcsa wwwrun xfs '_*'
+
+# ... unless we really want to.
+zstyle '*' single-ignored show
+
+  expand-or-complete-with-dots() {
+    # toggle line-wrapping off and back on again
+    [[ -n "$terminfo[rmam]" && -n "$terminfo[smam]" ]] && echoti rmam
+    print -Pn "%{%F{red}......%f%}"
+    [[ -n "$terminfo[rmam]" && -n "$terminfo[smam]" ]] && echoti smam
+
+    zle expand-or-complete
+    zle redisplay
+  }
+  zle -N expand-or-complete-with-dots
+  bindkey "^I" expand-or-complete-with-dots
