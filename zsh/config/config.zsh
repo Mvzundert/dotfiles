004c60f81bc81fe78a7ddcb7a64a06c730da41f9
Marius van Zundert <Marius.vanzundert@gmail.com>
2019-11-07 16:16:22 +0100

    Added no glob alias to prevent globbing while scp-ing

--- zsh/config/config.zsh
+++ zsh/config/config.zsh
@@ -29,3 +29,14 @@ else
     export EDITOR='vim'
 fi
 
+# Disable globbing on the remote path.
+alias scpng='noglob scp_wrap'
+function scp_wrap {
+  local -a args
+  local i
+  for i in "$@"; do case $i in
+    (*:*) args+=($i) ;;
+    (*) args+=(${~i}) ;;
+  esac; done
+  command scp "${(@)args}"
+}
