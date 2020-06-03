172aac716d7bf4cbe7512291c5666b01207f4d7f
Marius van Zundert <marius.vanzundert@cerberos.dev>
2020-06-03 11:11:16 +0200

    Added laravel artisan autocomplete

--- zsh/plugins/completion.zsh
+++ zsh/plugins/completion.zsh
@@ -64,3 +64,57 @@ fi
 if [[ $#hosts_completion -gt 0 ]]; then
     zstyle ':completion:*:(ssh|scp|rsync|slogin):*' hosts ${hosts_completion}
 fi
+
+function art() {
+    _artisan=`_artisan_find`
+
+    if [ "$_artisan" = "" ]; then
+        >&2 echo "Artisan: You seem to have upset the delicate internal balance of my housekeeper."
+        return 1
+    fi
+
+    _artisan_start_time=`date +%s`
+    php $_artisan $*
+    _artisan_exit_status=$? # Store the exit status so we can return it later
+
+    if [[ $1 = "make:"* && $ARTISAN_OPEN_ON_MAKE_EDITOR != "" ]]; then
+        # Find and open files created by artisan
+        _artisan_laravel_path=`dirname $_artisan`
+        find \
+            "$_artisan_laravel_path/app" \
+            "$_artisan_laravel_path/tests" \
+            "$_artisan_laravel_path/database" \
+            -type f \
+            -newermt "-$((`date +%s` - $_artisan_start_time + 1)) seconds" \
+            -exec $ARTISAN_OPEN_ON_MAKE_EDITOR {} \; 2>/dev/null
+    fi
+
+    return $_artisan_exit_status
+}
+
+compdef _artisan_add_completion artisan
+
+function _artisan_find() {
+    # Look for artisan up the file tree until the root directory
+    dir=.
+    until [ $dir -ef / ]; do
+        if [ -f "$dir/artisan" ]; then
+            echo "$dir/artisan"
+            return 0
+        fi
+
+        dir+=/..
+    done
+
+    return 1
+}
+
+function _artisan_add_completion() {
+    if [ "`_artisan_find`" != "" ]; then
+        compadd `_artisan_get_command_list`
+    fi
+}
+
+function _artisan_get_command_list() {
+    artisan --raw --no-ansi list | sed "s/[[:space:]].*//g"
+}
