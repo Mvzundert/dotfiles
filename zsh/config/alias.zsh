56b52cac6f3d5a6bb5b9e58acbe4c9e80239959b
Marius van Zundert <marius.vanzundert@cerberos.dev>
2020-12-10 11:30:31 +0100

    Added Kali docker alias

--- zsh/config/alias.zsh
+++ zsh/config/alias.zsh
@@ -91,6 +91,7 @@ alias dcirm='docker rmi $(docker images -q)'
 alias dcw='docker-compose exec workspace bash'
 alias dcs='docker-compose stop'
 alias dcu='docker-compose up -d'
+alias kali='docker-compose exec crb_kali /bin/bash'
 
 ############################
 # Git alias
