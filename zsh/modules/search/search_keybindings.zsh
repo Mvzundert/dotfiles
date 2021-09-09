7badf27da9f4b8b526ffd9bf1e2754e3ef3baced
Marius van Zundert <marius.vanzundert@cerberos.dev>
2021-09-09 13:06:13 +0200

    Moved Search to modules

---
+++ zsh/modules/search/search_keybindings.zsh
@@ -0,0 +1,5 @@
+# Make searching through previous commands more sane
+# We bind up and down arrow keys to search the history
+# forward or backward.
+bindkey '\e[A' history-search-backward
+bindkey '\e[B' history-search-forward
