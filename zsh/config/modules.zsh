6b77238c3081963edb3efe9af124e62e5b925ba1
Marius van Zundert <marius.vanzundert@cerberos.dev>
2021-09-09 13:55:42 +0200

    Made modules optional and refactored locations

--- zsh/config/modules.zsh
+++ zsh/config/modules.zsh
@@ -1,6 +1,8 @@
-#############################
+# -------------------------------------------------------------------
 # System Modules
-#############################
+# -------------------------------------------------------------------
+# These are always enabled
+
 # Search Module
 for file in $MODULES/search/*; do
     source "$file"
@@ -8,11 +10,6 @@ done
 
 # Editor Module
 for file in $MODULES/editors/*; do
-    source "$file"
-done
-
-# Tmux Module
-for file in $MODULES/tmux/*; do
     source "$file"
 done
 
@@ -36,54 +33,80 @@ for file in $MODULES/extract/*; do
     source "$file"
 done
 
-#############################
-# Coding plugins
-#############################
-# Git Module
-for file in $MODULES/development/git/*; do
-    source "$file"
-done
+source $ZSH/plugins/z.sh
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 
-# Build modules
-for file in $MODULES/development/build/*; do
-    source "$file"
-done
+# -------------------------------------------------------------------
+# Optional Modules
+# -------------------------------------------------------------------
+# These can enabled using the load file.
 
-# Build modules
-for file in $MODULES/docker/*; do
-    source "$file"
-done
+if dotfiles::check "${GIT}"; then
+  # Git Module
+  for file in $MODULES/development/git/*; do
+      source "$file"
+  done
+fi
+
+if dotfiles::check "${BUILD}"; then
+  # Build modules
+  for file in $MODULES/development/build/*; do
+      source "$file"
+  done
+fi
+
+if dotfiles::check "${DOCKER}"; then
+  # Build modules
+  for file in $MODULES/docker/*; do
+      source "$file"
+  done
+fi
+
+if dotfiles::check "${TMUX}"; then
+  # Tmux Module
+  for file in $MODULES/tmux/*; do
+      source "$file"
+  done
+fi
 
 #############################
 # Languages
 #############################
-# Python modules
-for file in $MODULES/development/python/*; do
-    source "$file"
-done
 
-# PHP modules
-for file in $MODULES/development/php/*; do
-    source "$file"
-done
+if dotfiles::check "${PHP}"; then
+  # PHP modules
+  for file in $MODULES/development/php/*; do
+      source "$file"
+  done
+fi
+
+if dotfiles::check "${PYTHON}"; then
+  # Python modules
+  for file in $MODULES/development/python/*; do
+      source "$file"
+  done
+fi
 
 #############################
 # Frameworks
 #############################
-# Laravel Modules
-for file in $MODULES/development/laravel/*; do
-    source "$file"
-done
+if dotfiles::check "${LARAVEL}"; then
+  # Laravel Modules
+  for file in $MODULES/development/laravel/*; do
+      source "$file"
+  done
+fi
 
-# Drupal
-for file in $MODULES/development/drupal/*; do
-    source "$file"
-done
+if dotfiles::check "${DRUPAL}}"; then
+  # Drupal
+  for file in $MODULES/development/drupal/*; do
+      source "$file"
+  done
+fi
 
-# Wordpress
-for file in $MODULES/development/wordpress/*; do
-    source "$file"
-done
-
-source $ZSH/plugins/z.sh
-source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+if dotfiles::check "${WORDPRESS}"; then
+  # Wordpress
+  for file in $MODULES/development/wordpress/*; do
+      source "$file"
+  done
+fi
