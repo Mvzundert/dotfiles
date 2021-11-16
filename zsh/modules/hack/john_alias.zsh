e2b8087e9a378f5b85d9454a195c949c5813ccd4
Marius van Zundert <marius.vanzundert@cerberos.dev>
2021-11-16 14:36:32 +0100

    Added John aliasses and Functions

---
+++ zsh/modules/hack/john_alias.zsh
@@ -0,0 +1,13 @@
+# -------------------------------------------------------------------
+# Compressed
+# -------------------------------------------------------------------
+alias rar2john="$(brew --cellar)/john-jumbo/1.9.0/share/john/rar2john"
+alias dmg2john="$(brew --cellar)/john-jumbo/1.9.0/share/john/dmg2john"
+alias zip2john="$(brew --cellar)/john-jumbo/1.9.0/share/john/zip2john"
+alias 7z2john="$(brew --cellar)/john-jumbo/1.9.0/share/john/7z2john"
+
+# -------------------------------------------------------------------
+# Password managers
+# -------------------------------------------------------------------
+alias keepass2john="$(brew --cellar)/john-jumbo/1.9.0/share/john/keepass2john"
+alias bitlocker2john="$(brew --cellar)/john-jumbo/1.9.0/share/john/bitlocker2john"
