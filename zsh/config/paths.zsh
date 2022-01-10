4aed2d00aa93e861c47ff8110fb27a1b14063df4
Marius van Zundert <marius.vanzundert@cerberos.dev>
2022-01-10 09:17:17 +0100

    WIP, always

--- zsh/config/paths.zsh
+++ zsh/config/paths.zsh
@@ -8,6 +8,7 @@ export PATH="$PATH:$HOME/.composer/vendor/bin"
 # -------------------------------------------------------------------
 export PATH=$HOME/bin:/usr/local/bin:$PATH
 export PATH="/usr/local/sbin:$PATH"
+export PATH="/usr/local/opt/curl/bin:$PATH"
 
 # -------------------------------------------------------------------
 # NVM and NPM
