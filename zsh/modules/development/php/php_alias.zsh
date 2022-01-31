271918bba192f252221158d619705f8eda5db371
Marius van Zundert <marius.vanzundert@cerberos.dev>
2022-01-31 14:40:59 +0100

    javiermon broke the regex, lame
    Signed-off-by: Marius van Zundert <marius.vanzundert@cerberos.dev>

--- zsh/modules/development/php/php_alias.zsh
+++ zsh/modules/development/php/php_alias.zsh
@@ -1,7 +1,7 @@
 alias cigr="composer info | ack"
 alias ciggr="composer global info | ack"
-alias test='$(get_test_install)'
-alias pest='./vendor/bin/pest'
+alias phpunit='$(get_phpunit_install)'
+alias stan='$(get_stan_install)'
 alias ci='composer install'
 alias cu='composer update'
 alias cda="composer dump-autoload"
