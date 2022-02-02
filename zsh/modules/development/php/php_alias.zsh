e6d8f8aac7e168a2a5c641c76c252ca4ddb9c270
Marius van Zundert <marius.vanzundert@cerberos.dev>
2022-02-02 10:38:09 +0100

    I __ a word
    Signed-off-by: Marius van Zundert <marius.vanzundert@cerberos.dev>

--- zsh/modules/development/php/php_alias.zsh
+++ zsh/modules/development/php/php_alias.zsh
@@ -1,7 +1,7 @@
 alias cigr="composer info | ack"
 alias ciggr="composer global info | ack"
 alias phpunit='$(get_phpunit_install)'
-alias stan='$(get_stan_install)'
+alias stan='$(get_stan_install) --memory-limit=2G'
 alias ci='composer install'
 alias cu='composer update'
 alias cda="composer dump-autoload"
