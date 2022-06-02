5bce341701bbe715170045d0278a7ff09538eca6
Marius van Zundert <marius.vanzundert@cerberos.dev>
2022-06-02 09:50:25 +0200

    Major fixup.

--- zsh/prompt/prompt.zsh
+++ zsh/prompt/prompt.zsh
@@ -6,7 +6,7 @@ PROMPT_SYMBOL='💀'
 # Update PROMPT and RPROMPT
 function update_prompt() {
     # Custom prompt with timestamp and git branch name
-    export PROMPT=$'%F{003}┌──[%B%F{003}%(6~.%-1~/…/%4~.%5~)%b%F{003}]-%(#.$PROMPT_SYMBOL.$PROMPT_SYMBOL)-%F{005}%*$(git_prompt_info)$(node_prompt)%B%F{003}%b%F{003}\n└─%B%(#.%F{003}#.%F{003}$)%b%F{003} '
+    export PROMPT=$'%F{002}┌──[%B%F{001}%(6~.%-1~/…/%4~.%5~)%b%F{002}]-%(#.$PROMPT_SYMBOL.$PROMPT_SYMBOL)-%F{005}%*$(git_prompt_info)$(node_prompt)%B%F{002}%b%F{002}\n└─%B%(#.%F{002}#.%F{002}$)%b%F{002} '
     export RPROMPT=''
 
     zle && zle reset-prompt
