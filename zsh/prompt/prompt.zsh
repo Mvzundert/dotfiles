c421e19693708af39ad7546d4d407ba097bb2adc
Marius van Zundert <marius.vanzundert@cerberos.dev>
2022-06-02 13:46:20 +0200

    bifurcation

--- zsh/prompt/prompt.zsh
+++ zsh/prompt/prompt.zsh
@@ -6,7 +6,7 @@ PROMPT_SYMBOL='💀'
 # Update PROMPT and RPROMPT
 function update_prompt() {
     # Custom prompt with timestamp and git branch name
-    export PROMPT=$'%F{002}┌──[%B%F{001}%(6~.%-1~/…/%4~.%5~)%b%F{002}]-%(#.$PROMPT_SYMBOL.$PROMPT_SYMBOL)-%F{005}%*$(git_prompt_info)$(node_prompt)%B%F{002}%b%F{002}\n└─%B%(#.%F{002}#.%F{002}$)%b%F{002} '
+    export PROMPT=$'%F{001}┌──[%B%F{004}%(6~.%-1~/…/%4~.%5~)%b%F{001}]-%(#.$PROMPT_SYMBOL.$PROMPT_SYMBOL)-%F{005}%*$(git_prompt_info)$(node_prompt)%B%F{001}%b%F{001}\n└─%B%(#.%F{001}#.%F{001}$)%b%F{001} '
     export RPROMPT=''
 
     zle && zle reset-prompt
