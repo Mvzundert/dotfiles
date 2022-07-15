16cf571945339dbf2daa716cc4893583b35a610c
Marius van Zundert <marius.vanzundert@cerberos.dev>
2022-07-15 17:27:00 +0200

    WIP - Torro Rosso

--- zsh/config/functions.zsh
+++ zsh/config/functions.zsh
@@ -8,30 +8,60 @@ function zsh_stats() {
     fc -l 1 | awk '{CMD[$2]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 "% " a;}' | grep -v "./" | column -c3 -s " " -t | sort -nr | nl |  head -n20
 }
 
-# Open files.
-function open_command() {
-    local open_cmd
+# Create Menu for choices on the prompt
+function choose_from_menu() {
+    local prompt="$1"
+    local output_variable="$2"
 
-    # define the open command
-    case "$OSTYPE" in
-        darwin*)  open_cmd='open' ;;
-        cygwin*)  open_cmd='cygstart' ;;
-        linux*)   ! [[ $(uname -a) =~ "Microsoft" ]] && open_cmd='xdg-open' || {
-            open_cmd='cmd.exe /c start ""'
-        [[ -e "$1" ]] && { 1="$(wslpath -w "${1:a}")" || return 1 }
-    } ;;
-msys*)    open_cmd='start ""' ;;
-*)        echo "Platform $OSTYPE not supported"
-    return 1
-    ;;
-  esac
+    shift
+    shift
 
-  # don't use nohup on OSX
-  if [[ "$OSTYPE" == darwin* ]]; then
-      ${=open_cmd} "$@" &>/dev/null
-  else
-      nohup ${=open_cmd} "$@" &>/dev/null
-  fi
+    local options=("$@")
+    local cur=0
+    local count=${#options[@]} index=0
+    local esc=$(echo -en "\e") # cache ESC as test doesn't allow esc codes
+
+    printf "$prompt\n"
+
+    while true; do
+        # list all options (option list is zero-based)
+        index=0
+
+        for o in "${options[@]}"; do
+            if [[ "$index" == "$cur" ]]; then
+                echo -e " >\e[7m$o\e[0m" # mark & highlight the current option
+            else
+                echo -e "  $o"
+            fi
+
+            index=$(( $index + 1 ))
+        done
+
+        # shellcheck disable=SC2162
+        read -s -n3 key # wait for user to key in arrows or ENTER
+
+
+        # shellcheck disable=SC1087
+        if [[ $key == $esc[A ]]; then
+            # up arrow
+            cur=$(( $cur - 1 ))
+
+            [ "$cur" -lt 0 ] && cur=0
+        elif [[ $key == $esc[B ]]; then
+            # down arrow
+            cur=$(( $cur + 1 ))
+
+            [ "$cur" -ge "$count" ] && cur=$(( $count - 1 ))
+        elif [[ $key == "" ]]; then
+            # nothing, i.e the read delimiter - ENTER
+            break
+        fi
+
+        echo -en "\e[${count}A" # go up to the beginning to re-render
+    done
+
+    # export the selection to the requested output variable
+    printf -v $output_variable "${options[$cur]}"
 }
 
 ####################################
--- zsh/modules/hack/tor_functions.zsh
+++ zsh/modules/hack/tor_functions.zsh
@@ -1,29 +1,27 @@
 function torro() {
-    USAGE="\033[33mUsage:\033[0m torro [-h] [-w] [-e] [-d]
+    USAGE="\033[33mUsage:\033[0m torro [-h] [-u] [-d]
 
     \033[33mOptions:\033[0m
         \033[92m-h\033[0m    Show this help text.
-        \033[92m-w\033[0m    Select Wifi interface if present.
-        \033[92m-e\033[0m    Select Ethernet interface if present (not yet supported).
+        \033[92m-e\033[0m    Select Network interface and turn everything on.
         \033[92m-d\033[0m    Turn everything off.
     "
-    WIFI=false
-    ETHERNET=false
+
+    # shellcheck disable=SC2034
     INTERFACE=false
+    UP=false
     DOWN=false
 
-    while getopts ':hwed' option; do
+    while getopts ':hweud' option; do
         # shellcheck disable=SC2220
+        # shellcheck disable=SC2213
         case "$option" in
         h)
             echo -e "$USAGE"
             return 1
             ;;
-        w)
-            WIFI=true
-            ;;
-        e)
-            ETHERNET=true
+        u)
+            UP=true
             ;;
         d)
             DOWN=true
@@ -39,64 +37,86 @@ function torro() {
         return 1
     fi
 
-    echo 'Determining Interface'
+    if ! [[ ${UP} == false ]]; then
+#        echo 'Upping'
+#        echo 'Determining Interface'
+#        echo 'Enabling Proxy'
 
-    if ! [[ ${WIFI} == false ]]; then
-        if dotfiles::check find_wifi_network_device; then
-            INTERFACE=$("find_wifi_network_device")
-            # Set the other interface to false because reasons
-            ETHERNET=false
-        fi
+#        interface_list=$(networksetup -listallhardwareports | grep "Hardware Port:" | cut -f2 -d":")
+
+        interface_list=(
+            "een"
+            "two"
+            "3"
+            "fow"
+        )
+
+        choose_from_menu "Choose an interface: " interface "${interface_list[@]}"
+
+        echo $interface
+
+        return 1
     fi
 
-    if ! [[ ${ETHERNET} == false ]]; then
-        if dotfiles::check find_ethernet_network_device; then
-            echo "Ethernet not supported (yet)"
-            return 1
-            # INTERFACE=$("find_ethernet_network_device")
-            # Set the other interface to false because reasons
-            WIFI=false
-        fi
-    fi
+#    echo 'Determining Interface'
+
+#    if ! [[ ${WIFI} == false ]]; then
+#        if dotfiles::check find_wifi_network_device; then
+#            INTERFACE=$("find_wifi_network_device")
+#            # Set the other interface to false because reasons
+#            ETHERNET=false
+#        fi
+#    fi
+#
+#    if ! [[ ${ETHERNET} == false ]]; then
+#        if dotfiles::check find_ethernet_network_device; then
+#            echo "Ethernet not supported (yet)"
+#            return 1
+#            # INTERFACE=$("find_ethernet_network_device")
+#            # Set the other interface to false because reasons
+#            WIFI=false
+#        fi
+#    fi
 
     # Remove leading and trailing whitespace characters.
-    export BINDING="$(echo -e "${INTERFACE}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+#    export BINDING="$(echo -e "${INTERFACE}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
 
     # Ask for the administrator password upfront
-    sudo -v
+#    sudo -v
 
     # # Keep-alive: update existing `sudo` time stamp until finished
-    while true; do
-        sudo -n true
-        sleep 60
-        kill -0 "$$" || exit
-    done 2>/dev/null &
+#    while true; do
+#        sudo -n true
+#        sleep 60
+#        kill -0 "$$" || exit
+#    done 2>/dev/null &
 
-    echo 'Enabling Proxy'
+#    echo 'Enabling Proxy'
 
     # Let's roll
-    sudo networksetup -setsocksfirewallproxy "$BINDING" 127.0.0.1 9050 off
-    sudo networksetup -setsocksfirewallproxystate "$BINDING" on
-
-    echo 'Enabling Tor Service'
-    brew services start tor
-
-    echo 'Enabling Socks Proxy'
-    export ALL_PROXY="socks4://127.0.0.1:9050"
+#    sudo networksetup -setsocksfirewallproxy "$BINDING" 127.0.0.1 9050 off
+#    sudo networksetup -setsocksfirewallproxystate "$BINDING" on
+#
+#    echo 'Enabling Tor Service'
+#    brew services start tor
+#
+#    echo 'Enabling Socks Proxy'
+#    export ALL_PROXY="socks4://127.0.0.1:9050"
 }
 
 # -------------------------------------------------------------------
-# Find Wi-Fi
+# Find Network Devices
 # -------------------------------------------------------------------
-function find_wifi_network_device() {
-    networksetup -listallhardwareports | ack Hardware Port: | cut -f2 -d":" | ack Wi-Fi
-}
+function find_network_devices() {
+    # shellcheck disable=SC2034
+    # shellcheck disable=SC2207
+    interface_list=$(networksetup -listallhardwareports | grep "Hardware Port:" | cut -f2 -d":")
 
-# -------------------------------------------------------------------
-# Find Ethernet
-# -------------------------------------------------------------------
-function find_ethernet_network_device() {
-    networksetup -listallhardwareports | ack Hardware Port: | cut -f2 -d":" | ack Ethernet
+    # shellcheck disable=SC2206
+    interfaces=($interface_list)
+
+    # shellcheck disable=SC2128
+    echo "$interfaces"
 }
 
 # -------------------------------------------------------------------
