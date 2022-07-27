80b2e674bd2f4bd0ccf00a638ff1a3295b500bb5
Marius van Zundert <marius.vanzundert@cerberos.dev>
2022-07-27 15:41:48 +0200

    first blush

--- zsh/config/paths.zsh
+++ zsh/config/paths.zsh
@@ -1,20 +1,95 @@
 # -------------------------------------------------------------------
-# Composer path
+# Add home bin folder to PATH
 # -------------------------------------------------------------------
-export PATH="$PATH:$HOME/.composer/vendor/bin"
+HOME_BIN="$HOME/bin"
+
+if [[ -d $HOME_BIN ]]; then
+    if ! [[ :$PATH: == *:$HOME_BIN:* ]]; then
+        export PATH="$HOME_BIN:$PATH"
+    fi
+fi
 
 # -------------------------------------------------------------------
-# Local bin
+# Add local bin folder to PATH
 # -------------------------------------------------------------------
-export PATH=$HOME/bin:/usr/local/bin:$PATH
-export PATH="/usr/local/sbin:$PATH"
-export PATH="/usr/local/opt/curl/bin:$PATH"
-export PATH="/usr/local/opt/mysql-client/bin:$PATH"
+USER_LOCAL_BIN="/usr/local/bin"
+
+if [[ -d $USER_LOCAL_BIN ]]; then
+    if ! [[ :$PATH: == *:$USER_LOCAL_BIN:* ]]; then
+        export PATH="$USER_LOCAL_BIN:$PATH"
+    fi
+fi
 
 # -------------------------------------------------------------------
-# Python paths
+# Add local sbin folder to PATH
 # -------------------------------------------------------------------
-export PATH=/usr/local/opt/python@3.9/Frameworks/Python.framework/Versions/3.9/bin:$PATH
+USER_LOCAL_SBIN="/usr/local/sbin"
+
+if [[ -d $USER_LOCAL_SBIN ]]; then
+    if ! [[ :$PATH: == *:$USER_LOCAL_SBIN:* ]]; then
+        export PATH="$USER_LOCAL_SBIN:$PATH"
+    fi
+fi
+
+# -------------------------------------------------------------------
+# Add usr sbin folder to PATH
+# -------------------------------------------------------------------
+USER_SBIN="/usr/sbin"
+
+if [[ -d $USER_SBIN ]]; then
+    if ! [[ :$PATH: == *:$USER_SBIN:* ]]; then
+        export PATH="$USER_SBIN:$PATH"
+    fi
+fi
+
+# -------------------------------------------------------------------
+# Add composer bin folder to PATH
+# -------------------------------------------------------------------
+COMPOSER_BIN="$HOME/.composer/vendor/bin"
+
+if [[ -d $COMPOSER_BIN ]]; then
+    if ! [[ :$PATH: == *:$COMPOSER_BIN:* ]]; then
+        export PATH="$COMPOSER_BIN:$PATH"
+    fi
+fi
+
+# -------------------------------------------------------------------
+# Add ruby bin folder to PATH
+# -------------------------------------------------------------------
+RUBY_BIN="/usr/local/opt/ruby/bin"
+
+if [[ -d $RUBY_BIN ]]; then
+    if ! [[ :$PATH: == *:$RUBY_BIN:* ]]; then
+        export PATH="$RUBY_BIN:$PATH"
+    fi
+fi
+
+# -------------------------------------------------------------------
+# Add mysql-client bin folder to PATH
+# -------------------------------------------------------------------
+MYSQLCLIENT_BIN="/usr/local/opt/mysql-client/bin"
+
+if [[ -d $MYSQLCLIENT_BIN ]]; then
+    if ! [[ :$PATH: == *:$MYSQLCLIENT_BIN:* ]]; then
+        export PATH="$MYSQLCLIENT_BIN:$PATH"
+    fi
+fi
+
+# -------------------------------------------------------------------
+# Add python bin folder to PATH
+# -------------------------------------------------------------------
+PYTHON_BIN="/usr/local/opt/python@3.9/Frameworks/Python.framework/Versions/3.9/bin"
+
+if [[ -d $PYTHON_BIN ]]; then
+    if ! [[ :$PATH: == *:$PYTHON_BIN:* ]]; then
+        export PATH="$PYTHON_BIN:$PATH"
+    fi
+fi
+
+# -------------------------------------------------------------------
+# Yarn
+# -------------------------------------------------------------------
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 
 # -------------------------------------------------------------------
 # NVM and NPM
@@ -29,20 +104,3 @@ NVM_HOMEBREW="/usr/local/opt/nvm"
 [ -s "$NVM_HOMEBREW/etc/bash_completion.d/nvm" ] && . "$NVM_HOMEBREW/etc/bash_completion.d/nvm"
 
 export NPM_DIR="$HOME/.npm"
-
-# -------------------------------------------------------------------
-# Yarn
-# -------------------------------------------------------------------
-export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
-
-# -------------------------------------------------------------------
-# Ruby
-# -------------------------------------------------------------------
-# Add ruby bin folder to PATH
-RUBY_BIN="/usr/local/opt/ruby/bin"
-
-if [[ -d $RUBY_BIN ]]; then
-    if ! [[ :$PATH: == *:$RUBY_BIN:* ]]; then
-        export PATH="$RUBY_BIN:$PATH"
-    fi
-fi
