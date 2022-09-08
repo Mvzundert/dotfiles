07a756531fb707a3890a587584ce1b20f8184c72
Marius van Zundert <marius.vanzundert@cerberos.dev>
2022-09-08 19:24:57 +0200

    de-misunderestimating

--- zsh/modules/editors/editors_alias.zsh
+++ zsh/modules/editors/editors_alias.zsh
@@ -1,3 +1,8 @@
 # use nvim, but don't make me think about it
 alias vim="stty stop '' -ixoff; nvim"
 alias emacs="vim"
+
+# Intellij
+alias pst="phpstorm ."
+alias pyc="pycharm ."
+alias sub="subl ."
