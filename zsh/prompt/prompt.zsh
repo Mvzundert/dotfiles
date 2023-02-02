6d65f25b1beb0b7e84248a61ef637c4139e6f32d
Marius van Zundert <marius.vanzundert@cerberos.dev>
2023-02-02 13:23:47 +0100

    commit

--- zsh/prompt/prompt.zsh
+++ zsh/prompt/prompt.zsh
@@ -6,7 +6,7 @@ PROMPT_SYMBOL='💀'
 # Update PROMPT and RPROMPT
 function update_prompt() {
     # Custom prompt with timestamp and git branch name
-    export PROMPT=$'%F{002}┌──[%B%F{010}%(6~.%-1~/…/%4~.%5~)%b%F{002}]-%(#.$PROMPT_SYMBOL.$PROMPT_SYMBOL)$(node_prompt)%B%F{002}%b%F{002}\n└─%B%(#.%F{002}#.%F{002}$)%b%F{002} '
+    export PROMPT=$'%F{05d}┌──[%B%F{076}%(6~.%-1~/…/%4~.%5~)%b%F{05d}]-%(#.$PROMPT_SYMBOL.$PROMPT_SYMBOL)$(node_prompt)%B%F{05d}%b%F{05d}\n└─%B%(#.%F{05d}#.%F{05d}$)%b%F{05d} '
     export RPROMPT='%(1V.(%1v).)'
 
     zle && zle reset-prompt
