41f4f9848934a203309568d35babf967ab83a821
Marius van Zundert <marius.vanzundert@cerberos.dev>
2023-04-27 10:48:58 +0200

    forgot we're not using a smart language

--- nvim/lua/marzun/remap.lua
+++ nvim/lua/marzun/remap.lua
@@ -24,7 +24,7 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+-- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
