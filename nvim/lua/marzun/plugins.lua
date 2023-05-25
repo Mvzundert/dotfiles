51d8d117de5a25148e5c2fb6396ad035470df724
Marius van Zundert <marius.vanzundert@cerberos.dev>
2023-05-25 11:50:12 +0200

    Fixed what was broken.

--- nvim/lua/marzun/plugins.lua
+++ nvim/lua/marzun/plugins.lua
@@ -4,62 +4,70 @@
 vim.cmd.packadd('packer.nvim')
 
 return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
 
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        -- or                            , branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.0',
+		-- or                            , branch = '0.1.x',
+		requires = { { 'nvim-lua/plenary.nvim' } }
+	}
 
-    use({
-	"catppuccin/nvim", as = "catppuccin"
-    })
+	use {
+		"startup-nvim/startup.nvim",
+		requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+		config = function()
+			require "startup".setup(require "config.startup_nvim")
+		end
+	}
 
-    use("folke/zen-mode.nvim")
+	use({
+		"catppuccin/nvim", as = "catppuccin"
+	})
 
-    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-    use('theprimeagen/harpoon')
-    use('mbbill/undotree')
-    use('mfussenegger/nvim-lint')
-    use('j-hui/fidget.nvim')
+	use("folke/zen-mode.nvim")
 
-    use('nvim-lualine/lualine.nvim') -- Fancier statusline
-    use('lukas-reineke/indent-blankline.nvim') -- Add indentation guides even on blank lines
-    use('numToStr/Comment.nvim') -- "gc" to comment visual regions/lines
-    use('tpope/vim-sleuth') -- Detect tabstop and shiftwidth automatically
-    use('jxnblk/vim-mdx-js') -- Make vim understand MDX files.
+	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+	use('theprimeagen/harpoon')
+	use('mbbill/undotree')
+	use('mfussenegger/nvim-lint')
+	use('j-hui/fidget.nvim')
 
-    -- Git stuff
-    use('lewis6991/gitsigns.nvim')
-    use('tpope/vim-rhubarb')
+	use('nvim-lualine/lualine.nvim')        -- Fancier statusline
+	use('lukas-reineke/indent-blankline.nvim') -- Add indentation guides even on blank lines
+	use('numToStr/Comment.nvim')            -- "gc" to comment visual regions/lines
+	use('tpope/vim-sleuth')                 -- Detect tabstop and shiftwidth automatically
+	use('jxnblk/vim-mdx-js')                -- Make vim understand MDX files.
 
-   -- Git Copilot 
-    use('github/copilot.vim') -- Make Github Copilot be a thing
+	-- Git stuff
+	use('lewis6991/gitsigns.nvim')
+	use('tpope/vim-rhubarb')
 
-    -- Setup Fugitive and it's dependencies
-    use('tpope/vim-fugitive')
-      use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v1.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
+	-- Git Copilot
+	use('github/copilot.vim') -- Make Github Copilot be a thing
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-nvim-lua'},
+	-- Setup Fugitive and it's dependencies
+	use('tpope/vim-fugitive')
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v1.x',
+		requires = {
+			-- LSP Support
+			{ 'neovim/nvim-lspconfig' },
+			{ 'williamboman/mason.nvim' },
+			{ 'williamboman/mason-lspconfig.nvim' },
 
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},
-		  {'rafamadriz/friendly-snippets'},
-	  }
-  }
+			-- Autocompletion
+			{ 'hrsh7th/nvim-cmp' },
+			{ 'hrsh7th/cmp-buffer' },
+			{ 'hrsh7th/cmp-path' },
+			{ 'saadparwaiz1/cmp_luasnip' },
+			{ 'hrsh7th/cmp-nvim-lsp' },
+			{ 'hrsh7th/cmp-nvim-lua' },
+
+			-- Snippets
+			{ 'L3MON4D3/LuaSnip' },
+			{ 'rafamadriz/friendly-snippets' },
+		}
+	}
 end)
