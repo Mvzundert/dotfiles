c8cb165b0c39eb05a4a305efb8ffac25fa695b81
Marius van Zundert <marius.vanzundert@cerberos.dev>
2023-09-07 13:19:50 +0200

    added code

--- lazy-nvim/lua/plugins/theme.lua
+++ lazy-nvim/lua/plugins/theme.lua
@@ -1,10 +1,21 @@
 return {
-  "folke/tokyonight.nvim",
+      "folke/tokyonight.nvim",
+  lazy = true,
   opts = {
-    transparent = true,
-    styles = {
-      sidebars = "transparent",
-      floats = "transparent",
-    },
-  },
+        style = "night",
+        transparent = true,
+        light_style = "day", -- The theme is used when the background is set to light
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+        styles = {
+            -- Style to be applied to different syntax groups
+            -- Value is any valid attr-list value for `:help nvim_set_hl`
+            comments = { italic = true },
+            keywords = { italic = true },
+            functions = {},
+            variables = {},
+            -- Background styles. Can be "dark", "transparent" or "normal"
+            sidebars = "transparent", -- style for sidebars, see below
+            floats = "transparent", -- style for floating windows
+        },
+      },
 }
