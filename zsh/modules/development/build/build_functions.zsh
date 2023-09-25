138ef18b965dcced7faf00fe1062e60d84c0f208
Marius van Zundert <marius.vanzundert@cerberos.dev>
2023-09-25 10:22:40 +0200

    Added Cargo building

--- zsh/modules/development/build/build_functions.zsh
+++ zsh/modules/development/build/build_functions.zsh
@@ -1,20 +1,23 @@
-# -------------------------------------------------------------------
-# Check current directory asset building dependencies, install and build them# Compressed
-# -------------------------------------------------------------------
+# Check current directory asset building dependencies, install and build them
 function build_project_assets() {
     USAGE="\033[33mUsage:\033[0m build [-h] [-f] [-i] [-p] [-c] [-a]
+
 \033[33mOptions:\033[0m
     \033[92m-h\033[0m  Show this help text.
     \033[92m-f\033[0m  Fresh install, Remove vendor/node_modules and install everything.
     \033[92m-i\033[0m  Install everything.
     \033[92m-p\033[0m  Install everything as on production.
     \033[92m-c\033[0m  Install only composer dependencies.
-    \033[92m-a\033[0m  Install only npm/yarn packages.
+    \033[92m-a\033[0m  Install only npm packages.
     "
 
     unset COMPOSER_ARGUMENTS
+    unset CARGO_ARGUMENTS
 
     COMPOSER_INSTALL=true
+
+    CARGO_INSTALL=true
+
     PACKAGE_INSTALL=true
     PACKAGE_BUILD=true
 
@@ -58,19 +61,25 @@ function build_project_assets() {
     if [[ ${PRODUCTION} == true ]]; then
         COMPOSER_ARGUMENTS="--no-dev"
         PACKAGE_ARGUMENTS="prod"
+        CARGO_ARGUMENTS="--release"
 
         echo "Running as on production"
     fi
 
     if [[ ${REFRESH} == true ]]; then
-        # Check for composer install
+        # Check for Composer install
         if [[ -d "$(pwd)/vendor" ]] && [[ ${COMPOSER_INSTALL} == true ]]; then
             rm -rf "$(pwd)/vendor" && echo "Removed 'vendor' directory"
         fi
 
-        # Check for composer install
-        if [[ -d "$(pwd)/node_modules" ]] && [[ ${PACKAGE_INSTALL} == true ]]; then
+        # Check for NPM install
+        if [[ -d "$(pwd)/node_modules" ]] && [[ ${PACKAGE_BUILD} == true ]]; then
             rm -rf "$(pwd)/node_modules" && echo "Removed 'node_modules' directory"
+        fi
+
+        # Check for Cargo install
+        if [[ -d "$(pwd)/target" ]] && [[ ${CARGO_INSTALL} == true ]]; then
+            rm -rf "$(pwd)/target" && echo "Removed 'target' directory"
         fi
     fi
 
@@ -92,30 +101,48 @@ function build_project_assets() {
                 PACKAGE_MANAGER="yarn"
             fi
 
-            if [[ ${PACKAGE_INSTALL} == true ]]; then
+            # Check for Bun install
+#            if [[ -n "$(command -v bun)" ]]; then
+#                PACKAGE_MANAGER="bun --no-save"
+#            fi
+
+            if [[ ${PACKAGE_INSTALL} == true ]] || [[ ${REFRESH} == true ]]; then
                 # Install packages
                 eval $PACKAGE_MANAGER install
             fi
 
+            echo "Building with: ${PACKAGE_MANAGER}"
+
             eval $PACKAGE_MANAGER run $PACKAGE_ARGUMENTS
         fi
     fi
+
+    # Check for Cargo install file
+    if [[ -f Cargo.toml ]]; then
+        if [[ ${CARGO_INSTALL} == true ]]; then
+            cargo build $CARGO_ARGUMENTS
+        fi
+    fi
 }
-# -------------------------------------------------------------------
+
 # Check current directory assets building dependencies, install and watch them
-# -------------------------------------------------------------------
 function watch_project_assets() {
     # Check for npm/yarn install file
     if [[ -f package.json ]]; then
-        PACKAGE_MANAGER="yarn"
+        # Default to NPM
+        PACKAGE_MANAGER="npm"
 
-        # Check for NPM lock file else use YARN
-        if [[ -f package-lock.json ]]; then
-            PACKAGE_MANAGER="npm"
+        # Check for Yarn lock file
+        if [[ -f yarn.lock ]]; then
+            PACKAGE_MANAGER="yarn"
         fi
 
-        eval $PACKAGE_MANAGER run dev
+        # Check for Bun install
+        if [[ -n "$(command -v bun)" ]]; then
+            PACKAGE_MANAGER="bun"
+        fi
 
         eval $PACKAGE_MANAGER run watch
     fi
 }
+
