0d9ba51548123eda674d01ba6196c4508bc1c2fa
Marius van Zundert <marius.vanzundert@cerberos.dev>
2023-10-11 12:42:00 +0200

    Updated Lualine to be better in Nvim

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,15 @@
+return {
+    {
+      "nvim-lualine/lualine.nvim",
+      event = "VeryLazy",
+      opts = {
+        options = {
+            theme = "horizon",
+            section_separators = { left = '', right = '' },
+            component_separators = { left = '', right = '' },
+            icons_enabled = true,
+        },
+      },
+    }
+}
+
