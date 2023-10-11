300e348181a4c257bbfd116ffa9da7a3b1b35438
Marius van Zundert <marius.vanzundert@cerberos.dev>
2023-10-11 11:34:22 +0200

    Updated neotree config

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -44,7 +44,7 @@ return {
       filtered_items = {
         visible = true, -- when true, they will just be displayed differently than normal items
         hide_dotfiles = false,
-        hide_gitignored = true,
+        hide_gitignored = false,
         hide_hidden = false, -- only works on Windows for hidden files/directories
         hide_by_name = {
           --"node_modules"
@@ -54,11 +54,11 @@ return {
           --"*/src/*/tsconfig.json",
         },
         always_show = { -- remains visible even if other settings would normally hide it
-          --".gitignored",
+          ".gitignored",
         },
         never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
-          --".DS_Store",
-          --"thumbs.db"
+          ".DS_Store",
+          "thumbs.db"
         },
         never_show_by_pattern = { -- uses glob style patterns
           --".null-ls_*",
