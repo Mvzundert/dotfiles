6b2d66339c979adbd8f75976ebde3ceccefc28c2
Marius van Zundert <marius.vanzundert@gmail.com>
2024-01-03 23:16:42 +0100

    Fixed hawtkeys

--- nvim/lua/plugins/hawtkeys.lua
+++ nvim/lua/plugins/hawtkeys.lua
@@ -1,4 +1,14 @@
 return {
     "tris203/hawtkeys.nvim",
     config = true,
-}
+    opts = {
+        -- If you use whichkey.register with an alias eg wk.register
+        ["wk.register"] = {
+            method = "which_key",
+        },
+        -- If you use lazy.nvim's keys property to configure keymaps in your plugins
+        ["lazy"] = {
+            method = "lazy",
+        },
+    }
+ }
