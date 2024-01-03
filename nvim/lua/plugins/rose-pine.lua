cb816d7aa08d75774282490b1c5239dcb6784f9c
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-01-03 13:55:57 +0100

    Fixed indents

--- nvim/lua/plugins/rose-pine.lua
+++ nvim/lua/plugins/rose-pine.lua
@@ -1,6 +1,6 @@
 return {
-        "rose-pine/neovim",
-        lazy = false,
-        name = "rose-pine",
-        priority = 1000,
+    "rose-pine/neovim",
+    lazy = false,
+    name = "rose-pine",
+    priority = 1000,
 }
