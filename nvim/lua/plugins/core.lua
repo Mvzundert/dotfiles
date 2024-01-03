b08085834d59a03785b64aabd3ab26f40b0e412e
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-01-03 12:21:13 +0100

    Redid the way the rose pine theme works

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -3,6 +3,25 @@ return {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "rose-pine-moon",
+        opts = {
+            style = "rose-pine", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+            light_style = "day", -- The theme is used when the background is set to light
+            transparent = true, -- Enable this to disable setting the background color
+            terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+        styles = {
+            comments = { italic = true },
+            keywords = { italic = true },
+            functions = {},
+            variables = {},
+            sidebars = "transparent", -- style for sidebars, see below
+            floats = "transparent", -- style for floating windows
+        },
+        sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+        day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+        hide_inactive_statusline = true, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+        dim_inactive = true, -- dims inactive windows
+        lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
+        }
     }
   }
 }
