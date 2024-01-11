4608c93ce53d3058a749a4a498f80b4b27d1544e
Marius van Zundert <marius.vanzundert@gmail.com>
2024-01-11 17:17:26 +0100

    Fixed background color for rose-pine

--- nvim/lua/plugins/rose-pine.lua
+++ nvim/lua/plugins/rose-pine.lua
@@ -47,22 +47,5 @@ return {
             -- Alternatively, set all headings at once.
             -- headings = "subtle",
         },
-
-        highlight_groups = {
-            -- Comment = { fg = "foam" },
-            -- VertSplit = { fg = "muted", bg = "muted" },
-        },
-
-        before_highlight = function(group, highlight, palette)
-            -- Disable all undercurls
-            -- if highlight.undercurl then
-            --     highlight.undercurl = false
-            -- end
-            --
-            -- Change palette colour
-            -- if highlight.fg == palette.pine then
-            --     highlight.fg = palette.foam
-            -- end
-        end,
     },
 }
