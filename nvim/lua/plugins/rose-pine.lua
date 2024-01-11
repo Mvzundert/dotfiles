e4ba37781d49659af73a22e081a26a5956fcd46c
Marius van Zundert <marius.vanzundert@gmail.com>
2024-01-11 17:19:15 +0100

    Removed more unneeded stuff

--- nvim/lua/plugins/rose-pine.lua
+++ nvim/lua/plugins/rose-pine.lua
@@ -14,38 +14,5 @@ return {
             italic = true,
             transparency = true,
         },
-
-        groups = {
-            border = "muted",
-            link = "iris",
-            panel = "surface",
-
-            error = "love",
-            hint = "iris",
-            info = "foam",
-            warn = "gold",
-
-            git_add = "foam",
-            git_change = "rose",
-            git_delete = "love",
-            git_dirty = "rose",
-            git_ignore = "muted",
-            git_merge = "iris",
-            git_rename = "pine",
-            git_stage = "iris",
-            git_text = "rose",
-            git_untracked = "subtle",
-
-            headings = {
-                h1 = "iris",
-                h2 = "foam",
-                h3 = "rose",
-                h4 = "gold",
-                h5 = "pine",
-                h6 = "foam",
-            },
-            -- Alternatively, set all headings at once.
-            -- headings = "subtle",
-        },
     },
 }
