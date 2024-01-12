c291323b4b8790bba03036c4eb84fbdd07e741da
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-01-12 08:58:58 +0100

    Theming issues

--- nvim/lua/plugins/rose-pine.lua
+++ nvim/lua/plugins/rose-pine.lua
@@ -4,15 +4,15 @@ return {
     name = "rose-pine",
     priority = 1000,
     opts = {
-        variant = "moon", -- auto, main, moon, or dawn
-        dark_variant = "main", -- main, moon, or dawn
-        dim_inactive_windows = false,
-        extend_background_behind_borders = true,
-
-        styles = {
-            bold = true,
-            italic = true,
-            transparency = true,
-        },
+        -- variant = "moon", -- auto, main, moon, or dawn
+        -- dark_variant = "main", -- main, moon, or dawn
+        -- dim_inactive_windows = false,
+        -- extend_background_behind_borders = true,
+        --
+        -- -- styles = {
+        -- --     bold = true,
+        --     italic = true,
+        --     transparency = true,
+        -- },
     },
 }
