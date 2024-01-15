3fc49d29aaabfca898d388bf137b3ad4c481ae3d
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-01-15 21:22:31 +0100

    Added setting of default variant for rose pine theme

--- nvim/lua/plugins/rose-pine.lua
+++ nvim/lua/plugins/rose-pine.lua
@@ -4,7 +4,7 @@ return {
     name = "rose-pine",
     priority = 1000,
     opts = {
-        -- variant = "moon", -- auto, main, moon, or dawn
+        variant = "moon", -- auto, main, moon, or dawn
         -- dark_variant = "main", -- main, moon, or dawn
         -- dim_inactive_windows = false,
         -- extend_background_behind_borders = true,
