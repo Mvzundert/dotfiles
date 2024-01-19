39e532d15eb149de9481b006f1ba43e5a484cbc7
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-01-19 12:46:10 +0100

    Fixed transparent issue
    Signed-off-by: Marius van Zundert <marius.vanzundert@cerberos.dev>

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -5,7 +5,7 @@ return {
     priority = 1000,
     opts = {
         flavour = "macchiato",
-        transparent_background = true, -- disables setting the background color.
+        transparent_background = fals -- disables setting the background color.
         show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
         term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
         dim_inactive = {
