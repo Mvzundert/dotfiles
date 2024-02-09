e014a1064e08caa8fc217861e08373ae50dd7e2c
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-02-09 12:57:53 +0100

    Removed rose pine and added gruvbox

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -26,7 +26,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax", "rose-pine" } },
+  install = { colorscheme = { "tokyonight", "habamax"} },
   checker = { enabled = false }, -- automatically check for plugin updates
   performance = {
     rtp = {
