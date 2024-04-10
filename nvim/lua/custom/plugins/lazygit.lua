7625b43854e6c29f191ad16c17d87fb7e9e365b6
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-04-10 10:53:09 +0200

    More logical binding for lazygit

--- nvim/lua/custom/plugins/lazygit.lua
+++ nvim/lua/custom/plugins/lazygit.lua
@@ -15,7 +15,7 @@ return {
     -- setting the keybinding for LazyGit with 'keys' is recommended in
     -- order to load the plugin when the command is run for the first time
     keys = {
-      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+      { '<leader>gg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
     },
   },
 }
