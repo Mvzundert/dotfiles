7269971954294fb6eb1478d576bd8bffd8ee75c7
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-04-10 09:48:09 +0200

    Finished up migration to kickstart

---
+++ nvim/lua/custom/plugins/molokai.lua
@@ -0,0 +1,13 @@
+return {
+  'UtkarshVerma/molokai.nvim',
+  lazy = false,
+  name = 'molokai',
+  priority = 1000,
+  opts = {
+    transparent = true,
+    styles = {
+      sidebars = 'transparent',
+      floats = 'transparent',
+    },
+  },
+}
---
+++ nvim/lua/custom/plugins/rose-pine.lua
@@ -0,0 +1,18 @@
+return {
+  'rose-pine/neovim',
+  lazy = false,
+  name = 'rose-pine',
+  priority = 1000,
+  opts = {
+    variant = 'moon', -- auto, main, moon, or dawn
+    -- dark_variant = "main", -- main, moon, or dawn
+    -- dim_inactive_windows = false,
+    -- extend_background_behind_borders = true,
+    --
+    -- -- styles = {
+    -- --     bold = true,
+    --     italic = true,
+    --     transparency = true,
+    -- },
+  },
+}
