d389980f64473b2dcc80e0270cd443bc6c176d1f
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-04-15 18:45:37 +0200

    GRUVVVVVV BOX

--- nvim/lua/custom/plugins/lualine.lua
+++ nvim/lua/custom/plugins/lualine.lua
@@ -11,7 +11,7 @@ return {
     event = 'VeryLazy',
     opts = {
       options = {
-        theme = 'molokai',
+        theme = 'gruvbox',
         section_separators = { left = '', right = '' },
         component_separators = { left = '', right = '' },
         disabled_filetypes = {
