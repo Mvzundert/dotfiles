9620fbf074fc70064a278fd2ccf97448c0e62240
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-04-23 14:01:52 +0200

    Updated ensure installed languages for treesitter

--- nvim/lua/custom/plugins/treesitter.lua
+++ nvim/lua/custom/plugins/treesitter.lua
@@ -3,7 +3,18 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'bash',
+        'php',
+        'python',
+        'rust',
+        'javascript',
+        'lua',
+        'luadoc',
+        'markdown',
+        'vim',
+        'vimdoc',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
