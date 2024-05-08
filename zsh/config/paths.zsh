fa5bdcf61d6edb7e8330d3462edd884f65237461
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-05-08 10:26:26 +0200

    Fixed loading of NVM now with lazyloading

--- zsh/config/paths.zsh
+++ zsh/config/paths.zsh
@@ -127,25 +127,3 @@ OPAM_BIN="$home/.opam/opam-init/init.zsh"
 if [[ -r $OPAM_BIN ]]; then
     source $HOME/.opam/opam-init/init.zsh  > /dev/null 2> /dev/null
 fi
-
-# -------------------------------------------------------------------
-# NVM and NPM
-# -------------------------------------------------------------------
-export NPM_DIR="$HOME/.npm"
-
-export NVM_DIR="$HOME/.nvm"
-#  # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-#  # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-NODE_GLOBALS=(`find ~/.nvm/versions/node -maxdepth 3 -type l -wholename '*/bin/*' | xargs -n1 basename | sort | uniq`)
-NODE_GLOBALS+=(node nvm)
-
-_load_nvm() {
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-}
-
-for cmd in "${NODE_GLOBALS[@]}"; do
-eval "function ${cmd}(){ unset -f ${NODE_GLOBALS[*]}; _load_nvm; unset -f _load_nvm; ${cmd} \$@; }"
-done
-unset cmd NODE_GLOBALS
