3ecb0ed3aeff3231b3c95085f6b26ab24eccaea2
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-05-11 21:02:33 +0200

    Swapped back to TOKYOOOOO ! (need to build a switcher)

--- nvim/init.lua
+++ nvim/init.lua
@@ -200,7 +200,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', 'tokyonight-day', 'kanagawa', 'everforest' or 'gruvbox'.
-      vim.cmd.colorscheme 'everforest'
+      vim.cmd.colorscheme 'tokyonight-moon'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
