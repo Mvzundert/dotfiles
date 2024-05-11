0a14dbe71ecb3337edc5618c8121aa73c460903b
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-05-11 20:55:23 +0200

    Swapped Nvim, Lualine and Tmux to Everforest theme

--- nvim/lua/custom/plugins/lualine.lua
+++ nvim/lua/custom/plugins/lualine.lua
@@ -21,7 +21,7 @@ return {
     event = 'VeryLazy',
     opts = {
       options = {
-        theme = 'tokyonight', -- gruvbox, everforest
+        theme = 'everforest', -- gruvbox, everforest
         section_separators = { left = '', right = '' },
         component_separators = { left = '', right = '' },
         disabled_filetypes = {
