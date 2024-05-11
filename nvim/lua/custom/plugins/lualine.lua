01bfdc00df76fb8ef628736d84a49853b8121e44
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-05-11 20:50:04 +0200

    Updated lualine comments

--- nvim/lua/custom/plugins/lualine.lua
+++ nvim/lua/custom/plugins/lualine.lua
@@ -21,7 +21,7 @@ return {
     event = 'VeryLazy',
     opts = {
       options = {
-        theme = 'tokyonight',
+        theme = 'tokyonight', -- gruvbox, everforest
         section_separators = { left = '', right = '' },
         component_separators = { left = '', right = '' },
         disabled_filetypes = {
