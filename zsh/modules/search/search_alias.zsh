780dbaee257a5bd2ae843959c259689c1731acfc
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-05-13 14:23:47 +0200

    Better search FZF config

--- zsh/modules/search/search_alias.zsh
+++ zsh/modules/search/search_alias.zsh
@@ -1,1 +1,9 @@
-alias ag='alias | fzf --height 60% --border'
+fzf_aliases() {
+  alias | fzf \
+        --preview 'echo {}' \
+        --preview-window=up:3:hidden \
+        --border \
+        --height=50%
+}
+
+alias ag=fzf_aliases
