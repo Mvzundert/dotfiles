8928b2af14c021aabc043d3a3992e727f4b29223
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-05-15 21:02:46 +0200

    Added keymaps for trouble

--- nvim/lua/custom/coding/trouble.lua
+++ nvim/lua/custom/coding/trouble.lua
@@ -6,4 +6,8 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
   },
+  keys = {
+    { '<leader>to', '<cmd>TroubleToggle<cr>', desc = 'Open Trouble' },
+    { '<leader>tr', '<cmd>TroubleRefresh<cr>', desc = 'Refresh Trouble' },
+  },
 }
