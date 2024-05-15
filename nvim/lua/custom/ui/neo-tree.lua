b3a65f5652e6ff42077afaed011193aff99f36bc
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-05-15 16:31:51 +0200

    Fixed neotree not following me around

--- nvim/lua/custom/ui/neo-tree.lua
+++ nvim/lua/custom/ui/neo-tree.lua
@@ -35,6 +35,11 @@ return {
         },
         never_show = {},
       },
+      follow_current_file = {
+        enabled = true, -- This will find and focus the file in the active buffer every time
+        --               -- the current file is changed while the tree is open.
+        leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+      },
     },
   },
   keys = {
