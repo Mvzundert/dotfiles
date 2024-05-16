596d47f5b002c7822df0566bfd334e6955573e24
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-05-16 21:29:11 +0200

    Fixed: Better opt for vim.opt

--- nvim/plugin/options.lua
+++ nvim/plugin/options.lua
@@ -1,3 +1,5 @@
+local opt = vim.opt
+
 -- Set to true if you have a Nerd Font installed
 vim.g.have_nerd_font = true
 
@@ -5,99 +7,99 @@ vim.g.have_nerd_font = true
 vim.g.markdown_recommended_style = 0
 
 -- Make line numbers default
-vim.opt.number = true
+opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
-vim.opt.relativenumber = true
+opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
+opt.mouse = 'a'
 
 -- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
+opt.showmode = false
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.opt.clipboard = 'unnamedplus'
+opt.clipboard = 'unnamedplus'
 
 -- Enable break indent
-vim.opt.breakindent = true
+opt.breakindent = true
 
 -- Save undo history
-vim.opt.undofile = true
+opt.undofile = true
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+opt.ignorecase = true
+opt.smartcase = true
 
 -- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
+opt.signcolumn = 'yes'
 
 -- Decrease update time
-vim.opt.updatetime = 250
+opt.updatetime = 250
 
 -- Decrease mapped sequence wait time
 -- Displays which-key popup sooner
-vim.opt.timeoutlen = 300
+opt.timeoutlen = 300
 
 -- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+opt.splitright = true
+opt.splitbelow = true
 
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+opt.list = true
+opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
+opt.inccommand = 'split'
 
 -- Show which line your cursor is on
-vim.opt.cursorline = true
+opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+opt.scrolloff = 10
 
-vim.opt.autowrite = true -- Enable auto write
-vim.opt.completeopt = 'menu,menuone,noselect'
-vim.opt.conceallevel = 3 -- Hide * markup for bold and italic
-vim.opt.confirm = true -- Confirm to save changes before exiting modified buffer
-vim.opt.cursorline = true -- Enable highlighting of the current line
-vim.opt.colorcolumn = '180'
-vim.opt.expandtab = true -- Use spaces instead of tabs
-vim.opt.formatoptions = 'jcroqlnt' -- tcqj
-vim.opt.grepformat = '%f:%l:%c:%m'
-vim.opt.grepprg = 'rg --vimgrep'
-vim.opt.ignorecase = true -- Ignore case
-vim.opt.inccommand = 'nosplit' -- preview incremental substitute
-vim.opt.laststatus = 0
-vim.opt.pumblend = 10 -- Popup blend
-vim.opt.pumheight = 10 -- Maximum number of entries in a popup
-vim.opt.scrolloff = 8 -- Lines of context
-vim.opt.sessionoptions = { 'buffers', 'curdir', 'tabpages', 'winsize' }
-vim.opt.shiftround = true -- Round indent
-vim.opt.shiftwidth = 4 -- Size of an indent
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shortmess:append { W = true, I = true, c = true }
-vim.opt.sidescrolloff = 8 -- Columns of context
-vim.opt.smartcase = true -- Don't ignore case with capitals
-vim.opt.smartindent = true -- Insert indents automatically
-vim.opt.spelllang = { 'en' }
-vim.opt.termguicolors = true -- True color support
-vim.opt.undolevels = 10000
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.wildmode = 'longest:full,full' -- Command-line completion mode
-vim.opt.winminwidth = 5 -- Minimum window width
-vim.opt.wrap = false -- Disable line wrap
-vim.opt.viminfo = "'100,f1,<1000,s1000,h"
+opt.autowrite = true -- Enable auto write
+opt.completeopt = 'menu,menuone,noselect'
+opt.conceallevel = 3 -- Hide * markup for bold and italic
+opt.confirm = true -- Confirm to save changes before exiting modified buffer
+opt.cursorline = true -- Enable highlighting of the current line
+opt.colorcolumn = '180'
+opt.expandtab = true -- Use spaces instead of tabs
+opt.formatoptions = 'jcroqlnt' -- tcqj
+opt.grepformat = '%f:%l:%c:%m'
+opt.grepprg = 'rg --vimgrep'
+opt.ignorecase = true -- Ignore case
+opt.inccommand = 'nosplit' -- preview incremental substitute
+opt.laststatus = 0
+opt.pumblend = 10 -- Popup blend
+opt.pumheight = 10 -- Maximum number of entries in a popup
+opt.scrolloff = 8 -- Lines of context
+opt.sessionoptions = { 'buffers', 'curdir', 'tabpages', 'winsize' }
+opt.shiftround = true -- Round indent
+opt.shiftwidth = 4 -- Size of an indent
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shortmess:append { W = true, I = true, c = true }
+opt.sidescrolloff = 8 -- Columns of context
+opt.smartcase = true -- Don't ignore case with capitals
+opt.smartindent = true -- Insert indents automatically
+opt.spelllang = { 'en' }
+opt.termguicolors = true -- True color support
+opt.undolevels = 10000
+opt.swapfile = false
+opt.backup = false
+opt.wildmode = 'longest:full,full' -- Command-line completion mode
+opt.winminwidth = 5 -- Minimum window width
+opt.wrap = false -- Disable line wrap
+opt.viminfo = "'100,f1,<1000,s1000,h"
 
 if vim.fn.has 'nvim-0.9.0' == 1 then
-  vim.opt.splitkeep = 'screen'
-  vim.opt.shortmess:append { C = true }
+  opt.splitkeep = 'screen'
+  opt.shortmess:append { C = true }
 end
 
-vim.opt.hlsearch = true
+opt.hlsearch = true
