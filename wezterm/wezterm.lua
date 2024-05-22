3a1a7a15766382281c86546a71229fb63a4bd3be
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-05-22 11:38:29 +0200

    Added Wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,227 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- Alias action
+local act = wezterm.action
+
+-- Disale default keybindings as they interfere with Tmux
+config.disable_default_key_bindings = true
+
+-- We detect which mode (dark or light) the system is in and swap style accordingly
+function Get_appearance()
+	if wezterm.gui then
+		return wezterm.gui.get_appearance()
+	end
+	return "Dark"
+end
+
+function Scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Eldritch"
+	else
+		return "Eldritch"
+	end
+end
+
+config.color_scheme = Scheme_for_appearance(Get_appearance())
+
+-- Path to background image, must be full path
+-- config.window_background_image = "/full/path/to/image"
+
+config.window_background_image_hsb = {
+	-- Darken the background image by reducing it to 1/3rd
+	brightness = 0.1,
+
+	-- You can adjust the hue by scaling its value.
+	-- a multiplier of 1.0 leaves the value unchanged.
+	hue = 1.0,
+
+	-- You can adjust the saturation also.
+	saturation = 1.0,
+}
+
+-- Opacity of the window background, don't go higher then 1.0
+config.window_background_opacity = 0.8
+
+-- Everything Font related
+config.font = wezterm.font("Hack nerd font")
+config.font_size = 20.0
+
+-- Everything related to the tabbar
+config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = true
+
+-- Enable the tab bar, retro style
+config.colors = {
+	tab_bar = {
+		-- The color of the strip that goes along the top of the window
+		-- (does not apply when fancy tab bar is in use)
+		background = "#0b0022",
+
+		-- The active tab is the one that has focus in the window
+		active_tab = {
+			-- The color of the background area for the tab
+			bg_color = "#2b2042",
+			-- The color of the text for the tab
+			fg_color = "#c0c0c0",
+
+			-- Specify whether you want "Half", "Normal" or "Bold" intensity for the
+			-- label shown for this tab.
+			-- The default is "Normal"
+			intensity = "Half",
+
+			-- Specify whether you want "None", "Single" or "Double" underline for
+			-- label shown for this tab.
+			-- The default is "None"
+			underline = "None",
+
+			-- Specify whether you want the text to be italic (true) or not (false)
+			-- for this tab.  The default is false.
+			italic = false,
+
+			-- Specify whether you want the text to be rendered with strikethrough (true)
+			-- or not for this tab.  The default is false.
+			strikethrough = false,
+		},
+
+		-- Inactive tabs are the tabs that do not have focus
+		inactive_tab = {
+			bg_color = "#1b1032",
+			fg_color = "#808080",
+
+			-- The same options that were listed under the `active_tab` section above
+			-- can also be used for `inactive_tab`.
+		},
+
+		-- You can configure some alternate styling when the mouse pointer
+		-- moves over inactive tabs
+		inactive_tab_hover = {
+			bg_color = "#3b3052",
+			fg_color = "#909090",
+			italic = true,
+
+			-- The same options that were listed under the `active_tab` section above
+			-- can also be used for `inactive_tab_hover`.
+		},
+
+		-- The new tab button that let you create new tabs
+		new_tab = {
+			bg_color = "#1b1032",
+			fg_color = "#808080",
+
+			-- The same options that were listed under the `active_tab` section above
+			-- can also be used for `new_tab`.
+		},
+
+		-- You can configure some alternate styling when the mouse pointer
+		-- moves over the new tab button
+		new_tab_hover = {
+			bg_color = "#3b3052",
+			fg_color = "#909090",
+			italic = true,
+
+			-- The same options that were listed under the `active_tab` section above
+			-- can also be used for `new_tab_hover`.
+		},
+	},
+}
+
+-- Config keybindings
+config.keys = {
+	{ key = "Enter", mods = "CMD", action = act.ToggleFullScreen },
+	{ key = "+", mods = "SUPER", action = act.IncreaseFontSize },
+	{ key = "-", mods = "SUPER", action = act.DecreaseFontSize },
+	{ key = "0", mods = "SUPER", action = act.ResetFontSize },
+	{ key = "c", mods = "SUPER", action = act.CopyTo("Clipboard") },
+	{ key = "f", mods = "SUPER", action = act.Search("CurrentSelectionOrEmptyString") },
+	{ key = "h", mods = "SHIFT|CTRL", action = act.HideApplication },
+	{ key = "k", mods = "SUPER", action = act.SendString("clear\u{20}\n") },
+	{ key = "l", mods = "SHIFT|CTRL", action = act.ShowDebugOverlay },
+	{ key = "n", mods = "SUPER", action = act.SpawnWindow },
+	{ key = "p", mods = "SHIFT|CTRL", action = act.ActivateCommandPalette },
+	{ key = "q", mods = "SUPER", action = act.QuitApplication },
+	{ key = "r", mods = "SHIFT|CTRL", action = act.ReloadConfiguration },
+	{ key = "t", mods = "SUPER", action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "v", mods = "SUPER", action = act.PasteFrom("Clipboard") },
+	{ key = "w", mods = "SUPER", action = act.CloseCurrentTab({ confirm = true }) },
+	{ key = "x", mods = "SHIFT|CTRL", action = act.ActivateCopyMode },
+	{ key = "Copy", mods = "NONE", action = act.CopyTo("Clipboard") },
+	{ key = "Paste", mods = "NONE", action = act.PasteFrom("Clipboard") },
+}
+
+config.key_tables = {
+	copy_mode = {
+		{ key = "Tab", mods = "NONE", action = act.CopyMode("MoveForwardWord") },
+		{ key = "Tab", mods = "SHIFT", action = act.CopyMode("MoveBackwardWord") },
+		{ key = "Enter", mods = "NONE", action = act.CopyMode("MoveToStartOfNextLine") },
+		{ key = "Escape", mods = "NONE", action = act.CopyMode("Close") },
+		{ key = "Space", mods = "NONE", action = act.CopyMode({ SetSelectionMode = "Cell" }) },
+		{ key = "$", mods = "NONE", action = act.CopyMode("MoveToEndOfLineContent") },
+		{ key = "$", mods = "SHIFT", action = act.CopyMode("MoveToEndOfLineContent") },
+		{ key = ",", mods = "NONE", action = act.CopyMode("JumpReverse") },
+		{ key = "0", mods = "NONE", action = act.CopyMode("MoveToStartOfLine") },
+		{ key = ";", mods = "NONE", action = act.CopyMode("JumpAgain") },
+		{ key = "F", mods = "NONE", action = act.CopyMode({ JumpBackward = { prev_char = false } }) },
+		{ key = "F", mods = "SHIFT", action = act.CopyMode({ JumpBackward = { prev_char = false } }) },
+		{ key = "G", mods = "NONE", action = act.CopyMode("MoveToScrollbackBottom") },
+		{ key = "G", mods = "SHIFT", action = act.CopyMode("MoveToScrollbackBottom") },
+		{ key = "H", mods = "NONE", action = act.CopyMode("MoveToViewportTop") },
+		{ key = "H", mods = "SHIFT", action = act.CopyMode("MoveToViewportTop") },
+		{ key = "L", mods = "NONE", action = act.CopyMode("MoveToViewportBottom") },
+		{ key = "L", mods = "SHIFT", action = act.CopyMode("MoveToViewportBottom") },
+		{ key = "M", mods = "NONE", action = act.CopyMode("MoveToViewportMiddle") },
+		{ key = "M", mods = "SHIFT", action = act.CopyMode("MoveToViewportMiddle") },
+		{ key = "O", mods = "NONE", action = act.CopyMode("MoveToSelectionOtherEndHoriz") },
+		{ key = "O", mods = "SHIFT", action = act.CopyMode("MoveToSelectionOtherEndHoriz") },
+		{ key = "T", mods = "NONE", action = act.CopyMode({ JumpBackward = { prev_char = true } }) },
+		{ key = "T", mods = "SHIFT", action = act.CopyMode({ JumpBackward = { prev_char = true } }) },
+		{ key = "V", mods = "NONE", action = act.CopyMode({ SetSelectionMode = "Line" }) },
+		{ key = "V", mods = "SHIFT", action = act.CopyMode({ SetSelectionMode = "Line" }) },
+		{ key = "^", mods = "NONE", action = act.CopyMode("MoveToStartOfLineContent") },
+		{ key = "^", mods = "SHIFT", action = act.CopyMode("MoveToStartOfLineContent") },
+		{ key = "b", mods = "NONE", action = act.CopyMode("MoveBackwardWord") },
+		{ key = "b", mods = "ALT", action = act.CopyMode("MoveBackwardWord") },
+		{ key = "b", mods = "CTRL", action = act.CopyMode("PageUp") },
+		{ key = "c", mods = "CTRL", action = act.CopyMode("Close") },
+		{ key = "d", mods = "CTRL", action = act.CopyMode({ MoveByPage = 0.5 }) },
+		{ key = "e", mods = "NONE", action = act.CopyMode("MoveForwardWordEnd") },
+		{ key = "f", mods = "NONE", action = act.CopyMode({ JumpForward = { prev_char = false } }) },
+		{ key = "f", mods = "ALT", action = act.CopyMode("MoveForwardWord") },
+		{ key = "f", mods = "CTRL", action = act.CopyMode("PageDown") },
+		{ key = "g", mods = "NONE", action = act.CopyMode("MoveToScrollbackTop") },
+		{ key = "g", mods = "CTRL", action = act.CopyMode("Close") },
+		{ key = "h", mods = "NONE", action = act.CopyMode("MoveLeft") },
+		{ key = "j", mods = "NONE", action = act.CopyMode("MoveDown") },
+		{ key = "k", mods = "NONE", action = act.CopyMode("MoveUp") },
+		{ key = "l", mods = "NONE", action = act.CopyMode("MoveRight") },
+		{ key = "m", mods = "ALT", action = act.CopyMode("MoveToStartOfLineContent") },
+		{ key = "o", mods = "NONE", action = act.CopyMode("MoveToSelectionOtherEnd") },
+		{ key = "q", mods = "NONE", action = act.CopyMode("Close") },
+		{ key = "t", mods = "NONE", action = act.CopyMode({ JumpForward = { prev_char = true } }) },
+		{ key = "u", mods = "CTRL", action = act.CopyMode({ MoveByPage = -0.5 }) },
+		{ key = "v", mods = "NONE", action = act.CopyMode({ SetSelectionMode = "Cell" }) },
+		{ key = "v", mods = "CTRL", action = act.CopyMode({ SetSelectionMode = "Block" }) },
+		{ key = "w", mods = "NONE", action = act.CopyMode("MoveForwardWord") },
+		{
+			key = "y",
+			mods = "NONE",
+			action = act.Multiple({ { CopyTo = "ClipboardAndPrimarySelection" }, { CopyMode = "Close" } }),
+		},
+		{ key = "PageUp", mods = "NONE", action = act.CopyMode("PageUp") },
+		{ key = "PageDown", mods = "NONE", action = act.CopyMode("PageDown") },
+		{ key = "End", mods = "NONE", action = act.CopyMode("MoveToEndOfLineContent") },
+		{ key = "Home", mods = "NONE", action = act.CopyMode("MoveToStartOfLine") },
+		{ key = "LeftArrow", mods = "NONE", action = act.CopyMode("MoveLeft") },
+		{ key = "LeftArrow", mods = "ALT", action = act.CopyMode("MoveBackwardWord") },
+		{ key = "RightArrow", mods = "NONE", action = act.CopyMode("MoveRight") },
+		{ key = "RightArrow", mods = "ALT", action = act.CopyMode("MoveForwardWord") },
+		{ key = "UpArrow", mods = "NONE", action = act.CopyMode("MoveUp") },
+		{ key = "DownArrow", mods = "NONE", action = act.CopyMode("MoveDown") },
+	},
+}
+
+-- and finally, return the configuration to wezterm
+return config
