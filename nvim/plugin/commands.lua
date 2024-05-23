a38283cd9b6fa3f882cafdbb5b9e25730dd9ec35
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-05-23 13:58:15 +0200

    Added autodark

--- nvim/plugin/commands.lua
+++ nvim/plugin/commands.lua
@@ -3,9 +3,9 @@ local cmd = vim.cmd
 -- If you want to see what colorschemes are already installed,
 -- you can use `:Telescope colorscheme`.
 --
--- Load the colorscheme here.
+-- Load the colorscheme here, when Autodark is not active
 -- Like many other themes, this one has different styles, and you could load
 -- any other, such as 'tokyonight-storm', 'tokyonight-moon', 'tokyonight-day', 'kanagawa', 'everforest', 'eldritch 'or 'gruvbox'.
-cmd.colorscheme 'tokyonight-moon'
+-- cmd.colorscheme 'tokyonight-moon'
 
 cmd.hi 'Comment gui=none'
