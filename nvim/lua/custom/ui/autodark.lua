24ef7bce891a5f22e1ec363f02ab5579ece813eb
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-05-24 09:21:31 +0200

    Formatting

--- nvim/lua/custom/ui/autodark.lua
+++ nvim/lua/custom/ui/autodark.lua
@@ -2,10 +2,12 @@ return {
   'f-person/auto-dark-mode.nvim',
   opts = {
     update_interval = 1000,
+
     set_dark_mode = function()
       vim.api.nvim_set_option('background', 'dark')
       vim.cmd.colorscheme 'tokyonight-moon'
     end,
+
     set_light_mode = function()
       vim.api.nvim_set_option('background', 'light')
       vim.cmd.colorscheme 'tokyonight-day'
