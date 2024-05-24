3e32c5ba523220a7c9f81d0701a793dfac17b6ef
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-05-24 16:26:22 +0200

    (for)ever in the forest

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -38,11 +38,11 @@ function Scheme_for_appearance(appearance)
 	if appearance:find("Dark") then
 		-- Dark theme gets to be transparent
 		config.window_background_opacity = 0.8
-		return "Tokyo Night (Gogh)"
+		return "Everforest Dark (Gogh)"
 	else
 		-- Light theme gets unreadable when transparent
 		config.window_background_opacity = 0.9
-		return "Tokyo Night Light (Gogh)"
+		return "Everforest Light (Gogh)"
 	end
 end
 
