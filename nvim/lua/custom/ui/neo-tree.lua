71d25ab9be7eca45e1ac3034ce371acddffa3e0e
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-06-28 08:48:46 +0200

    Disabled image preview in neotree

--- nvim/lua/custom/ui/neo-tree.lua
+++ nvim/lua/custom/ui/neo-tree.lua
@@ -5,7 +5,7 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
-    '3rd/image.nvim', -- Optional image support in preview window: See `# Preview Mode` for more information
+    --    '3rd/image.nvim', -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   opts = {
     auto_clean_after_session_restore = true, -- Automatically clean up broken neo-tree buffers saved in sessions
