7b7f1a2f6f77ae82bb733045a07c640d7d58ee8b
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-08-12 11:10:47 +0200

    Updated docker compose aliases

--- zsh/modules/docker/docker_alias.zsh
+++ zsh/modules/docker/docker_alias.zsh
@@ -1,11 +1,11 @@
 alias dockers='open -a Docker'
 alias dcrm='docker rm $(docker ps -a -q)'
 alias dcirm='docker rmi $(docker images -q)'
-alias dcw='docker-compose exec workspace bash'
-alias dcs='docker-compose stop'
-alias dcu='docker-compose up -d'
-alias dcuv='docker-compose up'
-alias dcb='docker-compose build'
+alias dcw='docker compose exec workspace bash'
+alias dcs='docker compose stop'
+alias dcu='docker compose up -d'
+alias dcuv='docker compose up'
+alias dcb='docker compose build'
 alias dcrn='docker exec -it crb_dock_nginx bash -c "nginx -s reload"'
 
 # -------------------------------------------------------------------
