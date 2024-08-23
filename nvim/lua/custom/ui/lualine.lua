4b128890ebc38b0d7ca79229ae4150415309d8c0
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-08-23 11:18:30 +0200

    Updated lualine to be more usefull

--- nvim/lua/custom/ui/lualine.lua
+++ nvim/lua/custom/ui/lualine.lua
@@ -44,6 +44,17 @@ return {
           {
             'diff',
             colored = true, -- Displays a colored diff status if set to true
+            diff_color = {
+              -- Same color values as the general color option can be used here.
+              added = 'LuaLineDiffAdd', -- Changes the diff's added color
+              modified = 'LuaLineDiffChange', -- Changes the diff's modified color
+              removed = 'LuaLineDiffDelete', -- Changes the diff's removed color you
+            },
+            symbols = { added = '+', modified = '~', removed = '-' }, -- Changes the symbols used by the diff.
+            source = nil, -- A function that works as a data source for diff.
+            -- It must return a table as such:
+            --   { added = add_count, modified = modified_count, removed = removed_count }
+            -- or nil on failure. count <= 0 won't be displayed.
           },
           'diagnostics',
         },
