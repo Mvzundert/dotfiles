4075ddb96f2ff5f018c34726d5e07c80fcc132c0
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-09-24 13:33:31 +0200

    Removed unneeded keymaps for buffers

--- nvim/lua/custom/ui/buffers.lua
+++ nvim/lua/custom/ui/buffers.lua
@@ -11,8 +11,6 @@ return {
       { '<leader>bl', '<Cmd>BufferLineCloseLeft<CR>', desc = 'Delete Buffers to the Left' },
       { '<S-h>', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
       { '<S-l>', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
-      { '[b', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
-      { ']b', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
     },
     opts = {
       options = {
