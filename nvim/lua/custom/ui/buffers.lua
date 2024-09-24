687d549e2db2f6ac8df93f4ca4780126f65da5e3
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-09-24 14:27:28 +0200

    Changed buffer stuff

--- nvim/lua/custom/ui/buffers.lua
+++ nvim/lua/custom/ui/buffers.lua
@@ -1,6 +1,8 @@
 return {
   {
     'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = 'nvim-tree/nvim-web-devicons',
     event = 'VeryLazy',
     keys = {
       { '<leader>b', '', desc = '+[B]uffers' },
@@ -14,11 +16,12 @@ return {
     },
     opts = {
       options = {
+        -- "slant" | "slope" | "thick" | "thin"
         separator_style = 'thin',
-      -- stylua: ignore
-      close_command = function(n) require("mini.bufremove").delete(n, false) end,
-      -- stylua: ignore
-      right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
+        -- stylua: ignore
+        close_command = function(n) require("mini.bufremove").delete(n, false) end,
+        -- stylua: ignore
+        right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
         diagnostics = 'nvim_lsp',
         always_show_bufferline = false,
         offsets = {
@@ -65,8 +68,8 @@ return {
         end,
         desc = 'Delete Buffer',
       },
-    -- stylua: ignore
-    { "<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
+      -- stylua: ignore
+      { "<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
     },
   },
 }
