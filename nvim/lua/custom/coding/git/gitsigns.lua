8dc200f69855181a9d9e14178cd65207bdd54dae
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-09-27 16:29:11 +0200

    Better git signs config

--- nvim/lua/custom/coding/git/gitsigns.lua
+++ nvim/lua/custom/coding/git/gitsigns.lua
@@ -4,11 +4,11 @@ return { -- Adds git related signs to the gutter, as well as utilities for manag
   opts = {
     current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
     signs = {
-      add = { text = '┃' },
-      change = { text = '┃' },
-      delete = { text = '_' },
+      add = { text = ' ' },
+      change = { text = ' ' },
+      delete = { text = ' ' },
       topdelete = { text = '‾' },
-      changedelete = { text = '~' },
+      changedelete = { text = ' ' },
       untracked = { text = '┆' },
     },
     signs_staged = {
@@ -22,12 +22,20 @@ return { -- Adds git related signs to the gutter, as well as utilities for manag
   },
   keys = {
     {
-      '<leader>gb',
+      '<leader>gsb',
       function()
         require('gitsigns').toggle_current_line_blame()
       end,
       mode = '',
       desc = 'Toggle git line blame',
+    },
+    {
+      '<leader>gsd',
+      function()
+        require('gitsigns').toggle_deleted()
+      end,
+      mode = '',
+      desc = 'Toggle git deleted',
     },
   },
 }
