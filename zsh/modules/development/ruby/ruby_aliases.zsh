9cefc05b5eb5bef21f52f4a4575125c6642c847b
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-10-09 09:27:33 +0200

    Added Kamal Docker image alias

---
+++ zsh/modules/development/ruby/ruby_aliases.zsh
@@ -0,0 +1,1 @@
+alias kamal='docker run -it --rm -v "${PWD}:/workdir" -v "/run/host-services/ssh-auth.sock:/run/host-services/ssh-auth.sock" -e SSH_AUTH_SOCK="/run/host-services/ssh-auth.sock" -v "$HOME/.ssh:/root/.ssh" -v /var/run/docker.sock:/var/run/docker.sock ghcr.io/basecamp/kamal:latest'
