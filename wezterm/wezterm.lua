2a3d976e9b04fd334bcaccb39476a911ed702679
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-10-18 14:12:45 +0200

    Stuff

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -40,7 +40,7 @@ end
 function Scheme_for_appearance(appearance)
 	if appearance:find("Dark") then
 		-- Opacity of the window background, don't go higher then 1.0
-		config.window_background_opacity = 1.0
+		config.window_background_opacity = 0.8
 		return "Everforest Dark (Gogh)"
 	else
 		-- Opacity of the window background, don't go higher then 1.0
