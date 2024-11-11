4598be9a75ae1d0785c8c3e8e69a186ad207763a
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-11-11 14:22:13 +0100

    Crap. Tonight is raid night and I am already late.

--- zsh/modules/development/php/php_alias.zsh
+++ zsh/modules/development/php/php_alias.zsh
@@ -1,5 +1,6 @@
 alias cigr="composer info | ack"
 alias ciggr="composer global info | ack"
+alias symsec="symfony security:check"
 alias phpunit='$(get_phpunit_install)'
 alias stan='$(get_stan_install) --memory-limit=2G'
 alias ci='composer install'
