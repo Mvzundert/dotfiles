c73b48df935c5e77c999a5f2913ac32fef83afa2
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-11-20 10:39:37 +0100

    Formatting

--- zsh/config/fzf.zsh
+++ zsh/config/fzf.zsh
@@ -4,15 +4,20 @@ eval "$(fzf --zsh)"
 
 # disable sort when completing `git checkout`
 zstyle ':completion:*:git-checkout:*' sort false
+
 # set descriptions format to enable group support
 # NOTE: don't use escape sequences here, fzf-tab will ignore them
 zstyle ':completion:*:descriptions' format '[%d]'
+
 # set list-colors to enable filename colorizing
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+
 # force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
 zstyle ':completion:*' menu no
+
 # preview directory's content with eza when completing cd
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
+
 # switch group using `<` and `>`
 zstyle ':fzf-tab:*' switch-group '<' '>'
 
