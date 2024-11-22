682263389d9c4ac83ea7626acfdac0f72cbe45ed
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-11-22 09:37:03 +0100

    NomNomNomNom

--- nvim/lua/custom/ui/snacks.lua
+++ nvim/lua/custom/ui/snacks.lua
@@ -98,7 +98,20 @@ return {
       }),
     },
     quickfile = { enabled = true },
-    statuscolumn = { enabled = false },
+    statuscolumn = {
+      enabled = false,
+      left = { 'mark', 'sign' }, -- priority of signs on the left (high to low)
+      right = { 'fold', 'git' }, -- priority of signs on the right (high to low)
+      folds = {
+        open = false, -- show open fold icons
+        git_hl = false, -- use Git Signs hl for fold icons
+      },
+      git = {
+        -- patterns to match Git signs
+        patterns = { 'GitSign', 'MiniDiffSign' },
+      },
+      refresh = 50, -- refresh at most every 50ms
+    },
     words = {
       enabled = true, -- enable/disable the plugin
       debounce = 200, -- time in ms to wait before updating
