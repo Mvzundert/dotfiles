24be4721725a4386ecab8b511a59422a5ffe3284
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-11-27 09:51:00 +0100

    Removed hardcoded Copilot path

--- nvim/lua/custom/coding/cmp/copilot.lua
+++ nvim/lua/custom/coding/cmp/copilot.lua
@@ -8,7 +8,7 @@ return {
     opts = {
       suggestion = { enabled = false },
       panel = { enabled = false },
-      copilot_node_command = vim.fn.expand '$HOME' .. '/.nvm/versions/node/v20.10.0/bin/node', -- Node.js version must be > 20.x
+      -- copilot_node_command = vim.fn.expand '$HOME' .. '/.nvm/versions/node/v20.10.0/bin/node', -- Node.js version must be > 20.x
       filetypes = {
         markdown = true,
         help = true,
