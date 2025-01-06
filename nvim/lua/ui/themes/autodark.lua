7a195a00e0ca3e1ace0bdfdbd975393618bff94b
Marius van Zundert <marius.vanzundert@cerberos.dev>
2025-01-06 17:44:29 +0100

    debug suff

--- nvim/lua/ui/themes/autodark.lua
+++ nvim/lua/ui/themes/autodark.lua
@@ -9,7 +9,7 @@ return {
 
     set_dark_mode = function()
       vim.api.nvim_set_option('background', 'dark')
-      vim.cmd.colorscheme 'catppuccin'
+      vim.cmd.colorscheme 'vague'
     end,
 
     set_light_mode = function()
