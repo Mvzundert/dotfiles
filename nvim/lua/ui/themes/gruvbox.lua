3dd731bd5ccf2f66f82b876ef901b35c6ede4e01
Marius van Zundert <marius.vanzundert@cerberos.dev>
2025-01-27 10:23:42 +0100

    Back to the forest

--- nvim/lua/ui/themes/gruvbox.lua
+++ nvim/lua/ui/themes/gruvbox.lua
@@ -19,7 +19,7 @@ return {
     invert_signs = false,
     invert_tabline = false,
     invert_intend_guides = false,
-    inverse = true, -- invert background for search, diffs, statuslines and errors
+    inverse = false, -- invert background for search, diffs, statuslines and errors
     contrast = '', -- can be "hard", "soft" or empty string
     palette_overrides = {},
     overrides = {},
