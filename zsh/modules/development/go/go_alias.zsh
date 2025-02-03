0ba181dba1ac510c29ffbe8c4dfc70cc305be4d2
Marius van Zundert <marius.vanzundert@cerberos.dev>
2025-02-03 08:36:37 +0100

    Added hugo alias

---
+++ zsh/modules/development/go/go_alias.zsh
@@ -0,0 +1,6 @@
+# -------------------------------------------------------------------
+# Hugo alias 
+# -------------------------------------------------------------------
+alias hsd="hugo server --buildDrafts --disableFastRender --navigateToChanged"
+alias hsw="hugo server --disableFastRender --navigateToChanged"
+alias hb="hugo build -d public"
