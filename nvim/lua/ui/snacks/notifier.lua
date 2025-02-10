891d78319829d11dd375b84c0c773fc1426c16c7
Marius van Zundert <marius.vanzundert@cerberos.dev>
2025-02-10 17:41:00 +0100

    Fixed: Notifier from snacks now does not throw errors because we stopped looking at the LSP

--- nvim/lua/ui/snacks/notifier.lua
+++ nvim/lua/ui/snacks/notifier.lua
@@ -4,7 +4,7 @@ return {
   lazy = false,
   opts = {
     notifier = {
-      enabled = false,
+      enabled = true,
       width = { min = 40, max = 0.4 },
       height = { min = 1, max = 0.6 },
       -- editor margin to keep free. tabline and statusline are taken into account automatically
@@ -29,20 +29,6 @@ return {
       top_down = true, -- place notifications from top to bottom
       date_format = '%R', -- time format for notifications
       refresh = 50, -- refresh at most every 50ms
-      -- LSP progress notification
-      vim.api.nvim_create_autocmd('LspProgress', {
-        ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
-        callback = function(ev)
-          local spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' }
-          vim.notify(vim.lsp.status(), 'info', {
-            id = 'lsp_progress',
-            title = 'LSP Progress',
-            opts = function(notif)
-              notif.icon = ev.data.params.value.kind == 'end' and ' ' or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
-            end,
-          })
-        end,
-      }),
     },
   },
 }
