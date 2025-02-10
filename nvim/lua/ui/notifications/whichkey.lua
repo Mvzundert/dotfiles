16fe3799b3b276eaeb34e26d69251cb08f813d9d
Marius van Zundert <marius.vanzundert@cerberos.dev>
2025-02-10 19:59:34 +0100

    Refactor: Whichkey plugin now uses new structure and has opts

--- nvim/lua/ui/notifications/whichkey.lua
+++ nvim/lua/ui/notifications/whichkey.lua
@@ -1,9 +1,10 @@
 return {
-  { -- Useful plugin to show you pending keybinds.
-    'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
-    end,
+  'folke/which-key.nvim',
+  event = 'VeryLazy',
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    preset = 'helix',
   },
 }
