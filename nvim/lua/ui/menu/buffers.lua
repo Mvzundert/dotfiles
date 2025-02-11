38ec4a7d0280a8ae8af5db6a3d70db2b3217fc3d
Marius van Zundert <marius.vanzundert@cerberos.dev>
2025-02-11 10:58:35 +0100

    Removed: Neo-tree reference in bufferline

--- nvim/lua/ui/menu/buffers.lua
+++ nvim/lua/ui/menu/buffers.lua
@@ -24,14 +24,6 @@ return {
         right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
         diagnostics = 'nvim_lsp',
         always_show_bufferline = false,
-        offsets = {
-          {
-            filetype = 'neo-tree',
-            text = 'Neo-tree',
-            highlight = 'Directory',
-            text_align = 'left',
-          },
-        },
       },
     },
     config = function(_, opts)
