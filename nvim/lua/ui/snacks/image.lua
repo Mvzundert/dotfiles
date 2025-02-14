48a87f94fcbc68193904144accf00572e5821fe8
Marius van Zundert <marius.vanzundert@cerberos.dev>
2025-02-14 15:31:43 +0100

    Fixed Snacks image bug

--- nvim/lua/ui/snacks/image.lua
+++ nvim/lua/ui/snacks/image.lua
@@ -3,7 +3,17 @@ return {
   ---@type snacks.Config
   opts = {
     image = {
-      force = true, -- try displaying the image, even if the terminal does not support it
+      formats = { 'png', 'jpg', 'jpeg', 'gif', 'bmp', 'webp', 'tiff', 'heic', 'avif', 'mp4', 'mov', 'avi', 'mkv', 'webm' },
+      force = false, -- try displaying the image, even if the terminal does not support it
+      markdown = {
+        -- enable image viewer for markdown files
+        -- if your env doesn't support unicode placeholders, this will be disabled
+        enabled = true,
+        max_width = 80,
+        max_height = 40,
+      },
+      -- window options applied to windows displaying image buffers
+      -- an image buffer is a buffer with `filetype=image`
       wo = {
         wrap = false,
         number = false,
