0c141f126e4bfa875175a4350a0e0d58c7cc0a5c
Marius van Zundert <marius.vanzundert@cerberos.dev>
2025-02-19 08:43:26 +0100

    Cleaned up unneeded functions

--- fish/functions/fzf_search_history.fish
+++
@@ -1,8 +0,0 @@
-# ~/.config/fish/functions/fzf_search_history.fish
-function fzf_search_history
-    history | fzf --preview "echo {}" | read -l selection
-    if test -n "$selection"
-        commandline $selection
-    end
-end
-bind \cb fzf_search_history  # Ctrl+B for history search
