e6a3dbc588cdcb537d25293d92c34fbd793ce5eb
Marius van Zundert <marius.vanzundert@cerberos.dev>
2025-02-19 13:38:26 +0100

    Moved fish and zsh to shell folder and moved nvim to tool folder

---
+++ shells/zsh/load.zsh
@@ -0,0 +1,19 @@
+# To enable a module: true, Disable: Leave empty
+PHP=true
+PYTHON=true
+ELIXIR=true
+RUBY=true
+GO=true
+
+LARAVEL=true
+DRUPAL=
+WORDPRESS=
+
+DOCKER=true
+BUILD=true
+GIT=true
+TMUX=true
+HACK=
+
+MARZUN=
+STARSHIP=true
