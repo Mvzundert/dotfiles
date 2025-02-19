c3e627cd0ca4a46b58cfd75c388dea2cb83d6f04
Marius van Zundert <marius.vanzundert@cerberos.dev>
2025-02-19 09:02:47 +0100

    Removed aliasses that are in __git_init

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -28,21 +28,6 @@ alias t='sesh connect $(sesh list -c | fzf --height 40% --border)'
 alias ytmp3="youtube-dl -x --audio-format mp3 $1"
 
 # -------------------------------------------------------------------
-# Git alias
-# -------------------------------------------------------------------
-alias gst="git status"
-alias gaa="git add --all"
-alias ga="git add"
-alias gcb="git checkout -b"
-alias gco="git checkout"
-alias gup="git pull"
-alias gfa="git fetch --all --prune"
-alias gcmsg="git commit -m"
-alias gp="git push"
-alias gpt="git push && git push --tags"
-alias guh='git_update_hooks'
-
-# -------------------------------------------------------------------
 # Github CLI
 # -------------------------------------------------------------------
 # Github PR's reviewing
@@ -76,40 +61,6 @@ alias ghr="gh workflow run"
 alias gsum="git submodule update --remote --merge"
 alias gprs="git pull --recurse-submodules"
 alias gsur="git submodule update --recursive --remote"
-
-# -------------------------------------------------------------------
-# Diff and graph's
-# -------------------------------------------------------------------
-alias gd="git diff"
-alias gds="git diff --staged"
-alias gdch="git -c core.fileMode=false diff"
-alias glgp="git log --stat --color -p"
-alias glol="git log --graph --abbrev-commit --decorate --date=relative --all"
-alias glog="git log --all --decorate --oneline --graph"
-alias glc="git log --decorate --oneline --graph --show-signature"
-alias gcount=" git shortlog -n -s -e"
-
-# -------------------------------------------------------------------
-# Checkout branches
-# -------------------------------------------------------------------
-alias gcod='git checkout develop'
-alias gcos='git checkout staging'
-alias gcom='git checkout master'
-
-# -------------------------------------------------------------------
-# Rebase all the things
-# -------------------------------------------------------------------
-alias grbd='git rebase develop'
-alias grbs='git rebase staging'
-alias grbm='git rebase master'
-alias grbs='git rebase --skip'
-alias grbc='git rebase --continue'
-alias grba='git rebase --abort'
-
-# -------------------------------------------------------------------
-# Stash all the things
-# -------------------------------------------------------------------
-alias gstash='git stash'
 
 # -------------------------------------------------------------------
 # for fun and games (Here be dragons)
