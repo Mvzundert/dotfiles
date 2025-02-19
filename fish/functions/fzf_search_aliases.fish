4dbad9623804691a8c12d9846e2e449ad878750e
Marius van Zundert <marius.vanzundert@cerberos.dev>
2025-02-19 08:43:26 +0100

    Cleaned up unneeded functions

--- fish/functions/fzf_search_aliases.fish
+++
@@ -1,8 +0,0 @@
-# ~/.config/fish/functions/fzf_search_aliases.fish
-function fzf_search_aliases
-    alias | fzf | read -l selection
-    if test -n "$selection"
-        commandline $selection
-    end
-end
-bind \cv fzf_search_aliases  # Ctrl+V for alias search
