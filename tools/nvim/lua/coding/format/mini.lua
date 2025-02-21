e59632375fa8bbb68648bce1c837f9091af2e203
Marius van Zundert <marius.vanzundert@cerberos.dev>
2025-02-21 15:39:46 +0100

    Mini commit with mini changes

--- tools/nvim/lua/coding/format/mini.lua
+++ tools/nvim/lua/coding/format/mini.lua
@@ -1,13 +1,20 @@
 return {
   'echasnovski/mini.nvim',
   config = function()
+    -- Animate stuff in vim
+    -- require('mini.animate').setup()
+
+    -- Highlight the current word under the cursor
+    require('mini.cursorword').setup()
+
     -- Better Around/Inside textobjects
-    --
     -- Examples:
     --  - va)  - [V]isually select [A]round [)]paren
     --  - yinq - [Y]ank [I]nside [N]ext [']quote
     --  - ci'  - [C]hange [I]nside [']quote
     require('mini.ai').setup { n_lines = 500 }
+
+    -- Move code around
     require('mini.move').setup {
       mappings = {
         -- Move visual selection in Visual mode using Option + Arrow keys
@@ -31,10 +38,12 @@ return {
     }
 
     -- Add/delete/replace surroundings (brackets, quotes, etc.)
-    --
     -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
     -- - sd'   - [S]urround [D]elete [']quotes
     -- - sr)'  - [S]urround [R]eplace [)] [']
     require('mini.surround').setup()
+
+    -- Show the indent lines
+    require('mini.indentscope').setup()
   end,
 }
