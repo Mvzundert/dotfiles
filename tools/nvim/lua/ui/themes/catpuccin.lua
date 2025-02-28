04266b9f10c05bbd99494ae13f37cbcfb237e486
Marius van Zundert <marius.vanzundert@cerberos.dev>
2025-02-28 09:13:01 +0100

    Catpuccin theme fixes and fish vars

--- tools/nvim/lua/ui/themes/catpuccin.lua
+++ tools/nvim/lua/ui/themes/catpuccin.lua
@@ -6,7 +6,7 @@ return {
     flavour = 'auto', -- latte, frappe, macchiato, mocha
     background = { -- :h background
       light = 'latte',
-      dark = 'mocha',
+      dark = 'macchiato',
     },
     transparent_background = true, -- disables setting the background color.
     show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
@@ -38,14 +38,52 @@ return {
     custom_highlights = {},
     default_integrations = true,
     integrations = {
+      aerial = true,
+      alpha = true,
       cmp = true,
+      dashboard = true,
+      flash = true,
+      fzf = true,
+      grug_far = true,
       gitsigns = true,
-      nvimtree = true,
-      treesitter = true,
-      notify = false,
-      mini = {
+      headlines = true,
+      illuminate = true,
+      indent_blankline = { enabled = true },
+      leap = true,
+      lsp_trouble = true,
+      mason = true,
+      markdown = true,
+      mini = true,
+      native_lsp = {
         enabled = true,
-        indentscope_color = '',
+        underlines = {
+          errors = { 'undercurl' },
+          hints = { 'undercurl' },
+          warnings = { 'undercurl' },
+          information = { 'undercurl' },
+        },
+      },
+      navic = { enabled = true, custom_bg = 'lualine' },
+      neotest = true,
+      neotree = true,
+      noice = true,
+      notify = true,
+      semantic_tokens = true,
+      snacks = true,
+      telescope = true,
+      treesitter = true,
+      treesitter_context = true,
+      which_key = true,
+    },
+    specs = {
+      {
+        'akinsho/bufferline.nvim',
+        optional = true,
+        opts = function(_, opts)
+          if (vim.g.colors_name or ''):find 'catppuccin' then
+            opts.highlights = require('catppuccin.groups.integrations.bufferline').get()
+          end
+        end,
       },
     },
   },
