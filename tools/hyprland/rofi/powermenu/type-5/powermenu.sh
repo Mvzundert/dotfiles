d5c2a01c2e07e67cef4c62932f1c3908579d9a54
Marius van Zundert <marius.vanzundert@gmail.com>
2025-03-16 03:06:10 +0100

    Fixed Rofi

--- tools/hyprland/rofi/powermenu/type-5/powermenu.sh
+++ tools/hyprland/rofi/powermenu/type-5/powermenu.sh
@@ -4,7 +4,7 @@
 ## Github : @adi1090x
 #
 ## Rofi   : Power Menu
-#
+
 ## Available Styles
 #
 ## style-1   style-2   style-3   style-4   style-5
@@ -14,19 +14,19 @@ dir="$HOME/.config/rofi/powermenu/type-5"
 theme='style-1'
 
 # CMDs
-lastlogin="`last $USER | head -n1 | tr -s ' ' | cut -d' ' -f5,6,7`"
-uptime="`uptime -p | sed -e 's/up //g'`"
-host=`hostname`
+lastlogin="$(last $USER | head -n1 | tr -s ' ' | cut -d' ' -f5,6,7)"
+uptime="$(uptime -p | sed -e 's/up //g')"
+host=$(hostname)
 
 # Options
-hibernate=''
-shutdown=''
-reboot=''
-lock=''
-suspend=''
-logout=''
-yes=''
-no=''
+hibernate=''
+shutdown=''
+reboot=''
+lock=''
+suspend='⏾'
+logout=' '
+yes=''
+no=''
 
 # Rofi CMD
 rofi_cmd() {
@@ -92,26 +92,26 @@ run_cmd() {
 # Actions
 chosen="$(run_rofi)"
 case ${chosen} in
-    $shutdown)
-		run_cmd --shutdown
-        ;;
-    $reboot)
-		run_cmd --reboot
-        ;;
-    $hibernate)
-		run_cmd --hibernate
-        ;;
-    $lock)
-		if [[ -x '/usr/bin/betterlockscreen' ]]; then
-			betterlockscreen -l
-		elif [[ -x '/usr/bin/i3lock' ]]; then
-			i3lock
-		fi
-        ;;
-    $suspend)
-		run_cmd --suspend
-        ;;
-    $logout)
-		run_cmd --logout
-        ;;
+$shutdown)
+	run_cmd --shutdown
+	;;
+$reboot)
+	run_cmd --reboot
+	;;
+$hibernate)
+	run_cmd --hibernate
+	;;
+$lock)
+	if [[ -x '/usr/bin/betterlockscreen' ]]; then
+		betterlockscreen -l
+	elif [[ -x '/usr/bin/i3lock' ]]; then
+		i3lock
+	fi
+	;;
+$suspend)
+	run_cmd --suspend
+	;;
+$logout)
+	run_cmd --logout
+	;;
 esac
