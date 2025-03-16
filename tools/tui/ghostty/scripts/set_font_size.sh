8901f03145c87cf4c5ecfd0f7ec9c2032a9e5088
Marius van Zundert <marius.vanzundert@cerberos.dev>
2025-03-16 15:28:59 +0100

    Added: Dynamically setting the font based on the OS

---
+++ tools/tui/ghostty/scripts/set_font_size.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Detect the operating system
+OS=$(uname)
+
+# Set default font size
+FONT_SIZE=18
+
+# Set font size based on the operating system
+case "$OS" in
+Linux)
+    FONT_SIZE=14
+    ;;
+Darwin)
+    FONT_SIZE=20
+    ;;
+*)
+    FONT_SIZE=18
+    ;;
+esac
+
+# Write the configuration file
+cat <<EOL >$DOTFILES/tools/tui/ghostty/fonts
+# Font size
+font-size=$FONT_SIZE
+font-family="IBM Plex Mono"
+font-feature=+liga
+cursor-style=block
+adjust-underline-position=4
+EOL
