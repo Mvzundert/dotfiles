9ca1218acc2670032345df0f9d1dfa26daef82fe
Marius van Zundert <marius.vanzundert@cerberos.dev>
2025-06-04 14:21:02 +0200

    Updated snacks to have the keymaps fit with wichkey displaying in the correct group

--- tools/nvim/lua/ui/snacks/snacks.lua
+++ tools/nvim/lua/ui/snacks/snacks.lua
@@ -16,13 +16,13 @@ return {
         vim.print = _G.dd -- Override print to use snacks for `:=` command
 
         -- Create some toggle mappings
-        Snacks.toggle.option('relativenumber', { name = 'Relative Number' }):map '<leader>ur'
-        Snacks.toggle.line_number():map '<leader>ul'
-        Snacks.toggle.treesitter():map '<leader>uT'
-        Snacks.toggle.inlay_hints():map '<leader>uh'
+        Snacks.toggle.option('relativenumber', { name = 'Relative Number' }):map '<leader>tr'
+        Snacks.toggle.line_number():map '<leader>tl'
+        Snacks.toggle.treesitter():map '<leader>tT'
+        Snacks.toggle.inlay_hints():map '<leader>th'
 
         -- Dimming
-        Snacks.toggle.dim():map '<leader>ud'
+        Snacks.toggle.dim():map '<leader>td'
       end,
     })
   end,
