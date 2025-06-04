7a4d7ddce38f029c693df4ce9873a9cd6c01d02c
Marius van Zundert <marius.vanzundert@cerberos.dev>
2025-06-04 17:51:08 +0200

    Added Treesitter as a dependency for Neorg and nvim-cmp for completions

--- tools/nvim/lua/system/organisation/neorg.lua
+++ tools/nvim/lua/system/organisation/neorg.lua
@@ -2,14 +2,18 @@ return {
   'nvim-neorg/neorg',
   lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
   version = '*', -- Pin Neorg to the latest stable release
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter', -- Required for syntax highlighting and folding
+  },
   config = function()
     require('neorg').setup {
       load = {
         ['core.defaults'] = {},
         ['core.concealer'] = {},
         ['core.summary'] = {},
-        -- ['core.presenter'] = {},
-        ['core.ui.calendar'] = {},
+        ['core.integrations.treesitter'] = {},
+        ['core.integrations.nvim-cmp'] = {},
+
         -- We use our own keybinds
         ['core.keybinds'] = {
           config = {
