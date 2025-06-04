1f94de2b3bb6cefc43257a503efc723de9685fa7
Marius van Zundert <marius.vanzundert@cerberos.dev>
2025-06-04 14:19:27 +0200

    Formatted and ordered keymaps

--- tools/nvim/plugin/keymaps.lua
+++ tools/nvim/plugin/keymaps.lua
@@ -5,73 +5,37 @@
 local set = vim.keymap.set
 
 -- =====================================================================
--- General
+-- Groups for keymaps only used for which key formatting
+-- =====================================================================
+set('n', '<leader>s', '', { desc = '+[S]earch' })
+set('n', '<leader>b', '', { desc = '+[B]uffers' })
+set('n', '<leader>o', '', { desc = '+[O]rganisation' })
+set('n', '<leader>g', '', { desc = '+[G]it and [G]oto Definitions' })
+set('n', '<leader>l', '', { desc = '+[L]azy Git' })
+set('n', '<leader>f', '', { desc = '+[F]ind' })
+set('n', '<leader>p', '', { desc = '+[P]review' })
+set('n', '<leader>r', '', { desc = '+[R]ename' })
+set('n', '<leader>c', '', { desc = '+[C]ode and [C]heck' })
+set('n', '<leader>t', '', { desc = '+[T]oggle' })
+set('n', '<leader>q', '', { desc = '+[Q]uit' })
+
+-- =====================================================================
+-- Find
 -- =====================================================================
 set('n', '<leader>fc', function()
   Snacks.picker.files { cwd = vim.fn.stdpath 'config' }
 end, { desc = '[F]ind [C]onfig Files' })
 
+-- =====================================================================
+-- Preview
+-- =====================================================================
 set('n', '<leader>pC', function()
   Snacks.picker.colorschemes()
 end, { desc = '[P]review [C]olor schemes' })
 
-set('n', '<leader>sh', function()
-  Snacks.picker.help()
-end, { desc = '[S]earch [H]elp' })
-
-set('n', '<leader>sk', function()
-  Snacks.picker.keymaps()
-end, { desc = '[S]earch [K]eymaps' })
-
-set('n', '<leader>sr', function()
-  Snacks.picker.registers()
-end, { desc = '[S]earch [R]egisters' })
-
-set('n', '<leader>sR', function()
-  Snacks.picker.resume()
-end, { desc = 'Resume' })
-
-set('n', '<leader>su', function()
-  Snacks.picker.undo()
-end, { desc = '[S]earch [U]ndo History' })
-
-set('n', '<leader>n', function()
-  Snacks.picker.notifications()
-end, { desc = 'Toggle [N]otifications' })
-
-set('n', '<leader>z', function()
-  Snacks.zen()
-end, { desc = 'Toggle [Z]en Mode' })
-
-set('n', '<leader>?', function()
+set('n', '<leader>p?', function()
   require('which-key').show { global = false }
-end, { desc = 'Buffer Local Keymaps (which-key)' })
-
---- =====================================================================
--- Hardtiem toggle
--- =====================================================================
-set('n', '<leader>ht', '<Cmd>Hardtime toggle<CR>', { desc = 'Toggle Hardtime' })
-
--- =====================================================================
--- Buffers
--- =====================================================================
-set('n', '<leader>b', '', { desc = '+[B]uffers' })
-set('n', '<leader>bp', '<Cmd>BufferLineTogglePin<CR>', { desc = 'Toggle Pin' })
-set('n', '<leader>bP', '<Cmd>BufferLineGroupClose ungrouped<CR>', { desc = 'Delete Non-Pinned Buffers' })
-set('n', '<leader>bo', '<Cmd>BufferLineCloseOthers<CR>', { desc = 'Delete Other Buffers' })
-set('n', '<leader>br', '<Cmd>BufferLineCloseRight<CR>', { desc = 'Delete Buffers to the Right' })
-set('n', '<leader>bl', '<Cmd>BufferLineCloseLeft<CR>', { desc = 'Delete Buffers to the Left' })
-set('n', '<S-h>', '<cmd>BufferLineCyclePrev<cr>', { desc = 'Prev Buffer' })
-set('n', '<S-l>', '<cmd>BufferLineCycleNext<cr>', { desc = 'Next Buffer' })
-
--- =====================================================================
--- Security
--- =====================================================================
--- Toggle cloak for env files
-set('n', '<leader>cc', '<cmd>:CloakToggle<cr>', { desc = 'Toggle Cloak for Env files' })
-
--- Toggle cloak for a current line
-set('n', '<leader>cpl', '<cmd>:CloakPreviewLine<cr>', { desc = 'Toggle Cloak preview for current line' })
+end, { desc = '[P]review Buffer Local Keymaps (which-key)' })
 
 -- =====================================================================
 -- Navigation
@@ -84,12 +48,16 @@ set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
 -- Open oil file picker
-set('n', '<leader>-', require('oil').toggle_float, { desc = 'Open Oil' })
+set('n', '<leader>-', require('oil').toggle_float, { desc = '[-] Open Oil' })
+
 -- Easier exit terminal mode
 set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
 -- quit
-set('n', '<leader>qa', '<cmd>qa<cr>', { desc = 'Quit All' })
+set('n', '<leader>qa', '<cmd>qa<cr>', { desc = '[Q]uit [A]ll' })
+
 -- save file
 set({ 'i', 'x', 'n', 's' }, '<C-s>', '<cmd>w<cr><esc>', { desc = 'Save File' })
 
@@ -108,8 +76,41 @@ set('n', '<leader>e', function()
 end, { desc = '[E]xplorer' })
 
 -- =====================================================================
+-- Toggles
+-- =====================================================================
+-- Toggle cloak for env files
+set('n', '<leader>tc', '<cmd>:CloakToggle<cr>', { desc = '[T]oggle Cloak for Env files' })
+
+-- Toggle Hardtime (gives you a hardtime for not using vim correctly)
+set('n', '<leader>th', '<Cmd>Hardtime toggle<CR>', { desc = '[T]oggle [H]ardtime' })
+
+set('n', '<leader>tb', function()
+  require('gitsigns').toggle_current_line_blame()
+end, { desc = '[T]oggle Git Line [B]lame' })
+
+set('n', '<leader>tn', function()
+  Snacks.picker.notifications()
+end, { desc = '[T]oggle [N]otifications' })
+
+set('n', '<leader>tz', function()
+  Snacks.zen()
+end, { desc = '[T]oggle [Z]en Mode' })
+
+-- =====================================================================
 -- Search
 -- =====================================================================
+set('n', '<leader>sh', function()
+  Snacks.picker.help()
+end, { desc = '[S]earch [H]elp' })
+
+set('n', '<leader>sk', function()
+  Snacks.picker.keymaps()
+end, { desc = '[S]earch [K]eymaps' })
+
+set('n', '<leader>sr', function()
+  Snacks.picker.registers()
+end, { desc = '[S]earch [R]egisters' })
+
 set('n', '<leader>sf', function()
   Snacks.picker.files()
 end, { desc = '[S]earch [F]iles' })
@@ -122,11 +123,56 @@ set('n', '<leader>sg', function()
   Snacks.picker.grep()
 end, { desc = '[S]earch [G]rep' })
 
+set('n', '<leader>su', function()
+  Snacks.picker.undo()
+end, { desc = '[S]earch [U]ndo History' })
+
+-- @TODO: Fix this location being hardcoded
+set('n', '<leader>so', function()
+  require('snacks').picker.files { cwd = vim.fn.expand '~/Documents/notes' }
+end, { desc = '[S]earch [O]rganisation Notes' })
+
 set('n', '<leader>/', function()
   Snacks.picker.grep_buffers()
-end, { desc = 'Search using grep in open buffers' })
+end, { desc = '[S]earch using grep in open buffers' })
 
-set('n', '<leader>gr', '<cmd>:GrugFar<cr>', { desc = 'Search with Grug' })
+set('n', '<leader>sr', '<cmd>:GrugFar<cr>', { desc = '[S]earch with G[R]ug' })
+
+set('n', '<leader>sd', function()
+  Snacks.picker.diagnostics()
+end, { desc = '[S]earch [D]iagnostics' })
+
+set('n', '<leader>sD', function()
+  Snacks.picker.diagnostics_buffer()
+end, { desc = '[S]earch Buffer [D]iagnostics' })
+
+set('n', '<leader>:', function()
+  Snacks.picker.command_history()
+end, { desc = '[S]earch Command History' })
+
+set('n', '<leader>sa', function()
+  Snacks.picker.autocmds()
+end, { desc = '[S]earch [A]utocmds' })
+
+set('n', '<leader>sc', function()
+  Snacks.picker.commands()
+end, { desc = '[S]earch [C]ommands' })
+
+--- =====================================================================
+-- Neorg
+-- =====================================================================
+set('n', '<leader>on', '<Cmd>Neorg<CR>', { desc = '[O]pen [N]eorg' })
+
+-- =====================================================================
+-- Buffers
+-- =====================================================================
+set('n', '<leader>bp', '<Cmd>BufferLineTogglePin<CR>', { desc = '[B]uffer Toggle [P]in' })
+set('n', '<leader>bP', '<Cmd>BufferLineGroupClose ungrouped<CR>', { desc = '[B]uffer Delete Non-[P]inned Buffers' })
+set('n', '<leader>bo', '<Cmd>BufferLineCloseOthers<CR>', { desc = '[B]uffer Delete [O]thers' })
+set('n', '<leader>br', '<Cmd>BufferLineCloseRight<CR>', { desc = '[B]uffer Delete to the [R]ight' })
+set('n', '<leader>bl', '<Cmd>BufferLineCloseLeft<CR>', { desc = '[B]uffer Delete to the [L]eft' })
+set('n', '<S-h>', '<cmd>BufferLineCyclePrev<cr>', { desc = 'Prev Buffer' })
+set('n', '<S-l>', '<cmd>BufferLineCycleNext<cr>', { desc = 'Next Buffer' })
 
 -- =====================================================================
 -- Git
@@ -155,10 +201,6 @@ set('n', '<leader>gf', function()
   Snacks.picker.git_log_file()
 end, { desc = 'Search [G]it log for current [F]ile' })
 
-set('n', '<leader>gbl', function()
-  require('gitsigns').toggle_current_line_blame()
-end, { desc = 'Toggle [G]it [L]ine [B]lame' })
-
 -- =====================================================================
 -- LSP
 -- =====================================================================
@@ -183,51 +225,12 @@ set('n', '<leader>gt', function()
 end, { desc = '[G]oto [T]ype Definitions' })
 
 -- =====================================================================
--- Todo, Trouble
+-- Check
 -- =====================================================================
 set('n', '<leader>ct', function()
   Snacks.picker.todo_comments()
 end, { desc = '[C]heck [T]odo' })
 
-set('n', '<leader>dt', '<cmd>Trouble diagnostics toggle<cr>', { desc = '[D]iagnostics [T]rouble)' })
+set('n', '<leader>cd', '<cmd>Trouble diagnostics toggle<cr>', { desc = '[C]heck [D]iagnostics' })
 
--- =====================================================================
--- Test
--- =====================================================================
-set('n', '<leader>tn', function()
-  require('neotest').run.run()
-end, { desc = 'Run Nearest' })
-
-set('n', '<leader>tl', function()
-  require('neotest').run.run_last()
-end, { desc = 'Run Last' })
-
-set('n', '<leader>ts', function()
-  require('neotest').summary.toggle()
-end, { desc = 'Toggle Summary' })
-
--- =====================================================================
--- Commands
--- =====================================================================
-set('n', '<leader>:', function()
-  Snacks.picker.command_history()
-end, { desc = 'Search Command History' })
-
-set('n', '<leader>sa', function()
-  Snacks.picker.autocmds()
-end, { desc = '[S]earch [A]utocmds' })
-
-set('n', '<leader>sc', function()
-  Snacks.picker.commands()
-end, { desc = '[S]earch [C]ommands' })
-
--- =====================================================================
--- Diagnostics
--- =====================================================================
-set('n', '<leader>sd', function()
-  Snacks.picker.diagnostics()
-end, { desc = '[S]earch [D]iagnostics' })
-
-set('n', '<leader>sD', function()
-  Snacks.picker.diagnostics_buffer()
-end, { desc = '[S]earch Buffer [D]iagnostics' })
+set('n', '<leader>cm', '<Cmd>Mason<CR>', { desc = '[C]heck [M]ason' })
