d58e46b559390a540584c0716d651846f6239fbd
Marius van Zundert <marius.vanzundert@cerberos.dev>
2025-06-04 14:19:17 +0200

    Added commands, api and diagnostics to commands plugin

--- tools/nvim/plugin/commands.lua
+++ tools/nvim/plugin/commands.lua
@@ -1,7 +1,12 @@
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
 local cmd = vim.cmd
+local api = vim.api
+local diagnostic = vim.diagnostic
+local set = vim.keymap.set
 
 -- If you want to see what colorschemes are already installed,
--- you can use `:Telescope colorscheme`.
+-- you can use ``.
 --
 -- Load the colorscheme here, when Autodark is not active
 -- Like many other themes, this one has different styles, and you could load
@@ -18,3 +23,64 @@ cmd [[
   hi TermNormal  guibg=NONE ctermbg=NONE
   hi TermNormalNC guibg=NONE ctermbg=NONE
 ]]
+
+-- Configure diagnostics
+diagnostic.config {
+  float = {
+    focusable = false,
+    style = 'minimal',
+    border = 'rounded',
+    source = 'always',
+    header = '',
+    prefix = '',
+  },
+}
+
+-- Auto-show diagnostics on hover
+api.nvim_create_autocmd('CursorHold', {
+  callback = function()
+    local opts = {
+      focusable = false,
+      close_events = { 'BufLeave', 'CursorMoved', 'InsertEnter', 'FocusLost' },
+      border = 'rounded',
+      source = 'always',
+      prefix = ' ',
+      scope = 'cursor',
+    }
+    diagnostic.open_float(nil, opts)
+  end,
+})
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- Create keymappings only when in norg files
+api.nvim_create_autocmd('Filetype', {
+  pattern = 'norg',
+  callback = function()
+    set('n', '<leader>on', '<Plug>(neorg.dirman.new-note)', { buffer = true }, { desc = '[O]rganise [N]ew note' })
+  end,
+})
+
+-- Auto-show diagnostics on hover
+api.nvim_create_autocmd('CursorHold', {
+  callback = function()
+    local opts = {
+      focusable = false,
+      close_events = { 'BufLeave', 'CursorMoved', 'InsertEnter', 'FocusLost' },
+      border = 'rounded',
+      source = 'always',
+      prefix = ' ',
+      scope = 'cursor',
+    }
+    diagnostic.open_float(nil, opts)
+  end,
+})
