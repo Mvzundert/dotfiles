2a2649919ef955bf42acc6e59a39dba90fa071b8
Marius van Zundert <marius.vanzundert@cerberos.dev>
2025-06-04 14:21:25 +0200

    Adjusted Whichkey to have a modern display instead of helix

--- tools/nvim/lua/ui/menu/whichkey.lua
+++ tools/nvim/lua/ui/menu/whichkey.lua
@@ -2,9 +2,7 @@ return {
   'folke/which-key.nvim',
   event = 'VeryLazy',
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-    preset = 'helix',
+    -- Classic, Modern, Helix
+    preset = 'modern',
   },
 }
