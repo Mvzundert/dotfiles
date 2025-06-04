8f23a1944dcb003eb8eb8ac02f64bb05fdcc9b0f
Marius van Zundert <marius.vanzundert@cerberos.dev>
2025-06-04 17:50:41 +0200

    Added Neorg parser to treesitter

--- tools/nvim/lua/coding/lsp/treesitter.lua
+++ tools/nvim/lua/coding/lsp/treesitter.lua
@@ -28,7 +28,6 @@ return {
     },
     config = function(_, opts)
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-
       local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
       parser_config.blade = {
         install_info = {
@@ -45,15 +44,23 @@ return {
         },
       }
 
+      parser_config.neorg = {
+        install_info = {
+          url = 'https://github.com/nvim-neorg/tree-sitter-norg',
+          files = { 'src/parser.c', 'src/scanner.cc' },
+          branch = 'main',
+        },
+        filetype = 'neorg',
+      }
+
+      vim.filetype.add {
+        pattern = {
+          ['*.norg'] = 'neorg',
+        },
+      }
+
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup(opts)
-
-      -- There are additional nvim-treesitter modules that you can use to interact
-      -- with nvim-treesitter. You should go explore a few and see what interests you:
-      --
-      --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-      --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
 }
