4780d13ca13ac141de8bf423c860750c59096de3
Marius van Zundert <marius.vanzundert@cerberos.dev>
2025-06-25 14:29:47 +0200

    Added: Tmux abbr to fish

--- shells/fish/conf.d/abbr.fish
+++ shells/fish/conf.d/abbr.fish
@@ -1,24 +1,16 @@
-# Abbreviations for Vim
+# --- Nvim Abbreviations ---
 abbr -a v nvim
 abbr -a vi nvim
 abbr -a vim nvim
 abbr -a nano nvim
 abbr -a emacs nvim
 
-# Navigation Abbreviations
+# --- Navigation Abbreviations ---
 abbr -a .. 'cd ..'
 abbr -a ... 'cd ../..'
 abbr -a .... 'cd ../../..'
 
 # --- Useful Tmux Abbreviations ---
-
-# List all current tmux sessions
-abbr tl 'tmux ls'
-
-# Rename the current tmux session (you already have this one, included for completeness)
-abbr trs 'tmux rename-session'
-
-# Kill the entire tmux server (closes all sessions)
-abbr tks 'tmux kill-server'
-
-# --- End Tmux Abbreviations ---
+abbr tl 'tmux ls' # List all current tmux sessions
+abbr trs 'tmux rename-session' # Rename the current tmux session (you already have this one, included for completeness)
+abbr tks 'tmux kill-server' # Kill the entire tmux server (closes all sessions)
