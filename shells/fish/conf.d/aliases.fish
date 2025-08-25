0d9d1cfdd25c63dbb394e50a1f332571f344c14c
Marius van Zundert <marius.vanzundert@cerberos.dev>
2025-08-25 11:25:32 +0200

    Added: Grep is ripgrep instead

--- shells/fish/conf.d/aliases.fish
+++ shells/fish/conf.d/aliases.fish
@@ -12,6 +12,7 @@ alias buuc='brew update && brew upgrade && brew cleanup'
 alias bbd='brew bundle dump --describe -f'
 alias t='sesh connect $(sesh list -c | fzf --height 40% --border)'
 alias cat='bat'
+alias grep='rg'
 
 # -------------------------------------------------------------------
 # Darwin only show and hide files
