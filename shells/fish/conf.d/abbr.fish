24295a326a551e91e061fc023e9efb637e6d7153
Marius van Zundert <marius.vanzundert@cerberos.dev>
2025-09-02 12:16:39 +0200

    Added: Zoxide abbr

--- shells/fish/conf.d/abbr.fish
+++ shells/fish/conf.d/abbr.fish
@@ -5,6 +5,14 @@ abbr -a vim nvim
 abbr -a nano nvim
 abbr -a emacs nvim
 
+# -- Zoxide all the things
+abbr --erase z &>/dev/null
+alias z=__zoxide_z
+
+abbr --erase zi &>/dev/null
+alias zi=__zoxide_zi
+
+
 # --- Navigation Abbreviations ---
 abbr -a .. 'cd ..'
 abbr -a ... 'cd ../..'
