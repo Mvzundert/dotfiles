eeea3fb8cc7da5c64cc874e52923017fe2f4c33f
Marius van Zundert <marius.vanzundert@cerberos.dev>
2025-10-14 09:46:37 +0200

    Fixed: PHP directory and CD script

--- shells/fish/functions/cd.fish
+++ shells/fish/functions/cd.fish
@@ -1,10 +1,9 @@
-# Overrides the default `cd` command to run
-# the PHP version check after changing directories.
+# NOTE: The variable $__fish_current_php_version is used to cache the active version.
+#       You will need to initialize this variable when your shell starts (see below).
 function cd
     # Call the original `cd` command with all arguments.
     builtin cd $argv
 
-    # Get the exit status of the `cd` command.
     set -l cd_status $status
 
     # Check for the .php-version file only if the `cd` command was successful.
@@ -12,11 +11,23 @@ function cd
         if test -f ./.php-version
             set -l target_version (cat .php-version)
 
-            # Call the main function silently.
-            switchphp -q "$target_version"
+            # Extract the major.minor version using the efficient string match
+            set -l major_minor_version (string match -r '^\d+\.\d+' "$target_version" | string trim)
+
+            # Check if the target version is different from the cached version.
+            # Only run the external 'switchphp' command if a change is needed.
+            if test "$major_minor_version" != "$__fish_current_php_version"
+                # Check if we actually found a version number
+                if test -n "$major_minor_version"
+                    # Call the main function silently.
+                    switchphp -q "$major_minor_version"
+                    # Update the global cache variable to prevent unnecessary calls next time
+                    set -g __fish_current_php_version "$major_minor_version"
+                    echo "PHP switched to $major_minor_version"
+                end
+            end
         end
     end
 
-    # Return the original exit status of the `cd` command.
     return $cd_status
 end
