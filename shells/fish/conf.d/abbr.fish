68f29aed946381d3ee877849f8111b19b1ed22a5
Marius van Zundert <marius.vanzundert@cerberos.dev>
2025-10-30 08:49:08 +0100

    :q!

--- shells/fish/conf.d/abbr.fish
+++ shells/fish/conf.d/abbr.fish
@@ -19,8 +19,8 @@ abbr -a ... 'cd ../..'
 abbr -a .... 'cd ../../..'
 
 abbr -a cy cal -mwy
-abbr -a c3 cal -3
-abbr -a c6 cal -n6
+abbr -a c3 cal -w3
+abbr -a c6 cal -wn6
 
 # --- Useful Tmux Abbreviations ---
 abbr tl 'tmux ls' # List all current tmux sessions
