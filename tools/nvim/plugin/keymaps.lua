13df3ccbe05feae3a2b43de1b11e491f931b56bd
Marius van Zundert <marius.vanzundert@cerberos.dev>
2026-01-08 18:31:32 +0100

    Update keymaps.lua

--- tools/nvim/plugin/keymaps.lua
+++ tools/nvim/plugin/keymaps.lua
@@ -7,7 +7,7 @@ local set = vim.keymap.set
 -- Groups for keymaps (Which-Key labels)
 -- =====================================================================
 
--- Normal Mode Groups (Show Everything)
+-- Normal Mode Groups (Full List)
 local n_groups = {
   { '<leader>s', '+[S]earch and [S]ort' },
   { '<leader>b', '+[B]uffers' },
@@ -27,7 +27,7 @@ local n_groups = {
   { '<leader>h', '+[H]ardware & Permissions' },
 }
 
--- Visual Mode Groups (Only show relevant ones)
+-- Visual Mode Groups (Relevant only)
 local v_groups = {
   { '<leader>s', '+[S]earch and [S]ort' },
   { '<leader>u', '+[U]tils (Data Manipulation)' },
@@ -43,36 +43,27 @@ for _, group in ipairs(v_groups) do
 end
 
 -- =====================================================================
--- Git & LazyGit
+-- Git, Goto & Rename
 -- =====================================================================
-set('n', '<leader>lg', function()
-  Snacks.lazygit()
-end, { desc = '[L]azy [G]it' })
+set('n', '<leader>lg', function() Snacks.lazygit() end, { desc = '[L]azy [G]it' })
+set('n', '<leader>rn', vim.lsp.buf.rename, { desc = '[R]ename [N]ow' })
+set('n', '<leader>gd', vim.lsp.buf.definition, { desc = '[G]oto [D]efinition' })
+set('n', '<leader>gD', vim.lsp.buf.declaration, { desc = '[G]oto [D]eclaration' })
+set('n', '<leader>gi', vim.lsp.buf.implementation, { desc = '[G]oto [I]mplementation' })
 
 -- =====================================================================
 -- Code and Check
 -- =====================================================================
-set('n', '<leader>ct', function()
-  Snacks.picker.todo_comments()
-end, { desc = '[C]heck [T]odo Comments' })
--- Visual: Search project for selected text
-set('v', '<leader>cs', function()
-  Snacks.picker.grep_visual()
-end, { desc = '[C]ode Search Selection' })
+set('n', '<leader>ct', function() Snacks.picker.todo_comments() end, { desc = '[C]heck [T]odo Comments' })
+set('v', '<leader>cs', function() Snacks.picker.grep_visual() end, { desc = '[C]ode Search Selection' })
 
 -- =====================================================================
 -- Find & Preview
 -- =====================================================================
-set('n', '<leader>fc', function()
-  Snacks.picker.files { cwd = vim.fn.stdpath 'config' }
-end, { desc = '[F]ind [C]onfig Files' })
-set('n', '<leader>pC', function()
-  Snacks.picker.colorschemes()
-end, { desc = '[P]review [C]olor schemes' })
+set('n', '<leader>fc', function() Snacks.picker.files { cwd = vim.fn.stdpath 'config' } end, { desc = '[F]ind [C]onfig Files' })
+set('n', '<leader>pC', function() Snacks.picker.colorschemes() end, { desc = '[P]review [C]olor schemes' })
 set('n', '<leader>pm', '<Cmd>Markview toggle<CR>', { desc = '[P]review [M]arkdown' })
-set('n', '<leader>p?', function()
-  require('which-key').show { global = false }
-end, { desc = '[P]review local keymaps' })
+set('n', '<leader>p?', function() require('which-key').show({ global = false }) end, { desc = '[P]review local keymaps' })
 
 -- =====================================================================
 -- Diagnostics (Trouble) - On X
@@ -84,50 +75,39 @@ set('n', '<leader>xq', '<cmd>Trouble qflist toggle<cr>', { desc = '[X] Diagnosti
 set('n', '<leader>xr', '<cmd>Trouble lsp_references toggle<cr>', { desc = '[X] Diagnostics [R]eferences' })
 
 -- =====================================================================
--- Navigation & Files
+-- Navigation & Better Indent
 -- =====================================================================
 set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-set('n', '<leader>-', function()
-  require('oil').toggle_float()
-end, { desc = '[-] Open Oil' })
+
+-- Better Indent (Visual Mode)
+set('v', '<', '<gv')
+set('v', '>', '>gv')
+
+set('n', '<leader>-', function() require('oil').toggle_float() end, { desc = '[-] Open Oil' })
 set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 set('n', '<leader>qa', '<cmd>qa<cr>', { desc = '[Q]uit [A]ll' })
 set({ 'i', 'x', 'n', 's' }, '<C-s>', '<cmd>w<cr><esc>', { desc = 'Save File' })
-set('n', '<leader>e', function()
-  Snacks.explorer()
-end, { desc = '[E]xplorer' })
+set('n', '<leader>e', function() Snacks.explorer() end, { desc = '[E]xplorer' })
 
 -- =====================================================================
 -- Toggles
 -- =====================================================================
 set('n', '<leader>tc', '<cmd>CloakToggle<cr>', { desc = '[T]oggle Cloak' })
 set('n', '<leader>th', '<Cmd>Hardtime toggle<CR>', { desc = '[T]oggle [H]ardtime' })
-set('n', '<leader>tb', function()
-  require('gitsigns').toggle_current_line_blame()
-end, { desc = '[T]oggle Git Blame' })
-set('n', '<leader>tz', function()
-  Snacks.zen()
-end, { desc = '[T]oggle [Z]en Mode' })
+set('n', '<leader>tb', function() require('gitsigns').toggle_current_line_blame() end, { desc = '[T]oggle Git Blame' })
+set('n', '<leader>tz', function() Snacks.zen() end, { desc = '[T]oggle [Z]en Mode' })
 
 -- =====================================================================
 -- Search and Sort
 -- =====================================================================
-set('n', '<leader>sf', function()
-  Snacks.picker.files()
-end, { desc = '[S]earch [F]iles' })
-set('n', '<leader>ss', function()
-  Snacks.picker.smart()
-end, { desc = '[S]earch [S]mart Files' })
-set('n', '<leader>sg', function()
-  Snacks.picker.grep()
-end, { desc = '[S]earch [G]rep' })
-set('n', '<leader>su', function()
-  Snacks.picker.undo()
-end, { desc = '[S]earch [U]ndo History' })
+set('n', '<leader>sf', function() Snacks.picker.files() end, { desc = '[S]earch [F]iles' })
+set('n', '<leader>ss', function() Snacks.picker.smart() end, { desc = '[S]earch [S]mart Files' })
+set('n', '<leader>sg', function() Snacks.picker.grep() end, { desc = '[S]earch [G]rep' })
+set('n', '<leader>su', function() Snacks.picker.undo() end, { desc = '[S]earch [U]ndo History' })
 
 -- Sorting (Normal)
 set('n', '<leader>ss', '<cmd>%!sort<cr>', { desc = '[S]ort [S]entire file' })
@@ -163,7 +143,7 @@ set('n', '<leader>ud', '<cmd>r !date +\\%F<cr>', { desc = '[U]tils Insert [D]ate
 set('n', '<leader>ut', '<cmd>r !date +\\%T<cr>', { desc = '[U]tils Insert [T]ime' })
 set('n', '<leader>uc', '<cmd>.!bc<cr>', { desc = '[U]tils [C]alculate Line' })
 set('n', '<leader>uh', '<cmd>r !hostname<cr>', { desc = '[U]tils Insert [H]ostname' })
-set('n', '<leader>ui', "<cmd>r !hostname -i | awk '{print $1}'<cr>", { desc = '[U]tils Insert [I]P Address' })
+set('n', '<leader>ui', '<cmd>r !hostname -i | awk \'{print $1}\'<cr>', { desc = '[U]tils Insert [I]P Address' })
 set('n', '<leader>us', '<cmd>%s/\\s\\+$//e<cr>', { desc = '[U]tils Strip Whitespace' })
 set('n', '<leader>un', '<cmd>vnew | setlocal buftype= buflisted<cr>', { desc = '[U]tils [N]ew Scratch' })
 set('n', '<leader>up', '<cmd>let @+ = expand("%:p")<cr><cmd>echo "Path copied: " . expand("%:p")<cr>', { desc = '[U]tils Copy Path' })
@@ -171,10 +151,10 @@ set('n', '<leader>up', '<cmd>let @+ = expand("%:p")<cr><cmd>echo "Path copied: "
 -- Smart Save for Scratch Buffers
 set('n', '<leader>uS', function()
   local buf_name = vim.api.nvim_buf_get_name(0)
-  if buf_name == '' then
-    vim.api.nvim_feedkeys(':saveas ' .. vim.fn.expand '%:p:h' .. '/', 'n', false)
+  if buf_name == "" then
+    vim.api.nvim_feedkeys(":saveas " .. vim.fn.expand("%:p:h") .. "/", "n", false)
   else
-    vim.cmd 'w'
+    vim.cmd('w')
   end
 end, { desc = '[U]tils [S]ave Scratch' })
 
@@ -188,8 +168,9 @@ set('v', '<leader>ul', ':!tr "[:upper:]" "[:lower:]"<cr>', { desc = '[U]tils Low
 set('v', '<leader>uu', ':!tr "[:lower:]" "[:upper:]"<cr>', { desc = '[U]tils Uppercase' })
 
 -- =====================================================================
--- Export
+-- Export & Organisation
 -- =====================================================================
+set('n', '<leader>oa', '<cmd>OrgAgenda<cr>', { desc = '[O]rganisation [A]genda' })
 set('n', '<leader>mp', function()
   local current_file_path = vim.api.nvim_buf_get_name(0)
   if current_file_path == '' or not current_file_path:match '%.md$' then
