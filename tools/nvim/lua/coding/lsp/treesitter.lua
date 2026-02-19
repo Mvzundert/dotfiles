dda4490b839afea9300796e7b59d13b25efec1f8
Marius van Zundert <marius.vanzundert@cerberos.dev>
2026-02-19 09:44:18 +0100

    Removed: breaking treesitter conf

--- tools/nvim/lua/coding/lsp/treesitter.lua
+++ tools/nvim/lua/coding/lsp/treesitter.lua
@@ -1,67 +1,30 @@
 return {
-  { -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    opts = {
-      ensure_installed = {
-        'bash',
-        'php',
-        'python',
-        'rust',
-        'go',
-        'javascript',
-        'lua',
-        'luadoc',
-        'markdown',
-        'vim',
-        'vimdoc',
-      },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  build = ':TSUpdate',
+  opts = {
+    ensure_installed = {
+      'bash',
+      'php',
+      'python',
+      'rust',
+      'go',
+      'javascript',
+      'lua',
+      'luadoc',
+      'markdown',
+      'vim',
+      'vimdoc',
     },
-    config = function(_, opts)
-      -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-      local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
-      parser_config.blade = {
-        install_info = {
-          url = 'https://github.com/EmranMR/tree-sitter-blade',
-          files = { 'src/parser.c' },
-          branch = 'main',
-        },
-        filetype = 'blade',
-      }
-
-      vim.filetype.add {
-        pattern = {
-          ['.*%.blade%.php'] = 'blade',
-        },
-      }
-
-      parser_config.neorg = {
-        install_info = {
-          url = 'https://github.com/nvim-neorg/tree-sitter-norg',
-          files = { 'src/parser.c', 'src/scanner.cc' },
-          branch = 'main',
-        },
-        filetype = 'neorg',
-      }
-
-      vim.filetype.add {
-        pattern = {
-          ['*.norg'] = 'neorg',
-        },
-      }
-
-      ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(opts)
-    end,
+    -- Autoinstall languages that are not installed
+    auto_install = true,
+    highlight = {
+      enable = true,
+      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+      --  If you are experiencing weird indenting issues, add the language to
+      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+      additional_vim_regex_highlighting = { 'ruby' },
+    },
+    indent = { enable = true, disable = { 'ruby' } },
   },
 }
