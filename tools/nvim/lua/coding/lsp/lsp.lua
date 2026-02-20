41b9fc159c0fdcfe217b6ce8a5b1f5ba53e01b18
Marius van Zundert <marius.vanzundert@cerberos.dev>
2026-02-20 13:38:17 +0100

    Removed: Fidget nvim because it's replaced

--- tools/nvim/lua/coding/lsp/lsp.lua
+++ tools/nvim/lua/coding/lsp/lsp.lua
@@ -7,10 +7,6 @@ return {
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
-      -- Useful status updates for LSP.
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
-
       -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
       -- used for completion, annotations and signatures of Neovim apis
       { 'folke/neodev.nvim', opts = {} },
